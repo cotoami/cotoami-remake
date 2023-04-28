ab0cfaf954392baf08a919ba08c13c9da31e4e15
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-04-28 18:40:25 +0900

    feat(desktop): add a barebone ui based on scalafui

---
+++ cotoami_desktop/ui/build.sbt
@@ -0,0 +1,37 @@
+import org.scalajs.linker.interface.ModuleSplitStyle
+
+ThisBuild / scalaVersion := "2.13.10"
+
+val circeVersion = "0.14.5"
+val slinkyVersion = "0.7.3"
+
+lazy val cotoami = project
+  .in(file("."))
+  .enablePlugins(ScalaJSPlugin)
+  .settings(
+    scalacOptions ++= Seq("-encoding", "utf-8", "-deprecation", "-feature"),
+
+    // We have a `main` method
+    scalaJSUseMainModuleInitializer := true,
+
+    // Emit modules in the most Vite-friendly way.
+    // For the best feedback loop with Vite,
+    // it is recommended to emit small modules for application code.
+    scalaJSLinkerConfig ~= {
+      _.withModuleKind(ModuleKind.ESModule)
+        .withModuleSplitStyle(
+          ModuleSplitStyle.SmallModulesFor(List("cotoami"))
+        )
+    },
+    libraryDependencies ++= Seq(
+      "org.scala-js" %%% "scalajs-dom" % "2.4.0",
+      "org.scala-js" %%% "scala-js-macrotask-executor" % "1.0.0",
+      "me.shadaj" %%% "slinky-web" % slinkyVersion,
+      "me.shadaj" %%% "slinky-hot" % slinkyVersion,
+      "io.circe" %%% "circe-core" % circeVersion,
+      "io.circe" %%% "circe-generic" % circeVersion,
+      "io.circe" %%% "circe-parser" % circeVersion,
+      "org.typelevel" %%% "cats-effect" % "3.4.5",
+      "org.scalatest" %%% "scalatest" % "3.2.9" % Test
+    )
+  )
---
+++ cotoami_desktop/ui/src/main/scala/cotoami/Main.scala
@@ -0,0 +1,74 @@
+package cotoami
+
+import scala.scalajs.js
+import scala.scalajs.LinkingInfo
+import org.scalajs.dom
+import org.scalajs.dom.URL
+
+import slinky.core._
+import slinky.core.facade.ReactElement
+import slinky.hot
+import slinky.web.html._
+
+import cotoami.FunctionalUI._
+
+object Main {
+
+  //
+  // MODEL
+  //
+
+  case class Model(messages: Seq[String], input: String)
+
+  def init(url: URL): (Model, Cmds[Msg]) = (Model(Seq.empty, ""), Seq.empty)
+
+  //
+  // UPDATE
+  //
+
+  sealed trait Msg
+  case class Input(input: String) extends Msg
+  case object Send extends Msg
+
+  def update(msg: Msg, model: Model): (Model, Cmds[Msg]) =
+    msg match {
+      case Input(input) =>
+        (model.copy(input = input), Seq.empty)
+
+      case Send =>
+        (
+          model.copy(messages = model.messages :+ model.input, input = ""),
+          Seq.empty
+        )
+    }
+
+  //
+  // VIEW
+  //
+
+  def view(model: Model, dispatch: Msg => Unit): ReactElement =
+    div(
+      h1("Welcome to Functional UI!"),
+      div(className := "message-input")(
+        input(
+          value := model.input,
+          onInput := (e => dispatch(Input(e.target.value)))
+        ),
+        button(onClick := (e => dispatch(Send)))("Send")
+      ),
+      div(className := "messages")(
+        model.messages.map(div(className := "message")(_))
+      )
+    )
+
+  def main(args: Array[String]): Unit = {
+    if (LinkingInfo.developmentMode) {
+      hot.initialize()
+    }
+
+    Browser.runProgram(
+      dom.document.getElementById("app"),
+      Program(init, view, update)
+    )
+  }
+}
