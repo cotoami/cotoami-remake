dd59dbfb36bbb085d60ba823bd7554c0bae1648b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-05-11 12:34:13 +0900

    feat(db): add db.error

---
+++ cotoami_db/src/db/error.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DatabaseError {
+    #[error("Invalid directory path: {0}")]
+    InvalidRootDir(PathBuf),
+
+    #[error("Invalid file path: {path:?} ({reason:?})")]
+    InvalidFilePath { path: PathBuf, reason: String },
+
+    #[error("Unexpected change number (expected {expected:?}, actual {actual:?})")]
+    UnexpectedChangeNumber { expected: i64, actual: i64 },
+}
