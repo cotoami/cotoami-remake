922db583f5be32ac3ff9e9727d823545f742d6e9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-05-16 12:49:48 +0900

    chore(db): add migrations/004_changelog

---
+++ cotoami_db/migrations/004_changelog/down.sql
@@ -0,0 +1,4 @@
+DROP INDEX changelog_remote;
+DROP INDEX changelog_remote_node_id;
+
+DROP TABLE changelog;
---
+++ cotoami_db/migrations/004_changelog/up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE changelog (
+  -- Serial number of a changelog entry based on SQLite ROWID
+  --
+  -- ROWID will be filled automatically with an unused integer, 
+  -- usually one more than the largest ROWID currently in use.
+  --
+  -- If it is possible for entries to be deleted (no, for the time being), 
+  -- we probably should add an `AUTOINCREMENT` keyword to prevent the reuse of ROWIDs 
+  -- from previously deleted rows. - https://www.sqlite.org/autoinc.html
+  --
+  -- When replicating a database in another node, that node must ensure to 
+  -- apply the changelog entries in the serial number order (state machine replication).
+  serial_number INTEGER NOT NULL PRIMARY KEY,
+  
+  -- Universally unique changelog ID
+  uuid TEXT NOT NULL UNIQUE,
+
+  -- a remote node ID to which this change belongs
+  -- NULL if it is a local change
+  remote_node_id TEXT,
+  remote_serial_number INTEGER,
+
+  -- Change enum value in JSON-serialized form
+  change TEXT NOT NULL,
+
+  -- Registration date in this database
+  inserted_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, -- UTC
+
+  FOREIGN KEY(remote_node_id) REFERENCES nodes(uuid) ON DELETE RESTRICT
+);
+
+-- https://sqlite.org/partialindex.html
+CREATE UNIQUE INDEX changelog_remote 
+ON changelog(remote_node_id, remote_serial_number) 
+WHERE 
+  remote_node_id IS NOT NULL AND 
+  remote_serial_number IS NOT NULL;
+
+CREATE INDEX changelog_remote_node_id ON changelog(remote_node_id);
