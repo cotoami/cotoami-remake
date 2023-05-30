13827ef4cccf95fbb60e3ee6d4e0d38096ca728f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-05-30 09:35:51 +0900

    feat(db): add db::ops::changelog_ops::import_change

--- cotoami_db/src/db/ops/changelog_ops.rs
+++ cotoami_db/src/db/ops/changelog_ops.rs
@@ -1,5 +1,6 @@
 //! Changelog related operations
 
+use crate::db::error::DatabaseError;
 use crate::db::op::*;
 use crate::models::changelog::{Change, ChangelogEntry, NewChangelogEntry};
 use crate::models::node::Node;
@@ -18,7 +19,7 @@ pub fn get(number: i64) -> impl ReadOperation<Option<ChangelogEntry>> {
     })
 }
 
-pub fn get_last_change_number(node_id: Id<Node>) -> impl ReadOperation<Option<i64>> {
+pub fn get_last_change_number<'a>(node_id: &'a Id<Node>) -> impl ReadOperation<Option<i64>> + 'a {
     use crate::schema::changelog::dsl::*;
     use diesel::dsl::max;
     read_op(move |conn| {
@@ -30,9 +31,7 @@ pub fn get_last_change_number(node_id: Id<Node>) -> impl ReadOperation<Option<i6
     })
 }
 
-pub fn log_change<'a>(
-    change: &'a Change,
-) -> impl Operation<WritableConnection, ChangelogEntry> + 'a {
+pub fn log_change(change: &Change) -> impl Operation<WritableConnection, ChangelogEntry> + '_ {
     // insert_new can't return the result directly since the value would
     // reference the local variable `change.new_changelog_entry()`
     composite_op::<WritableConnection, _, _>(move |ctx| {
@@ -49,5 +48,35 @@ pub fn insert_new<'a>(
             .values(new_entry)
             .get_result(conn.deref_mut())
             .map_err(anyhow::Error::from)
+    })
+}
+
+pub fn import_change<'a>(
+    parent_node_id: &'a Id<Node>,
+    log: &'a ChangelogEntry,
+) -> impl Operation<WritableConnection, ChangelogEntry> + 'a {
+    composite_op::<WritableConnection, _, _>(move |ctx| {
+        // check the serial number of the change
+        let last_number = get_last_change_number(parent_node_id).run(ctx)?;
+        let expected_number = last_number.map(|n| n + 1).unwrap_or(1);
+        if log.serial_number != expected_number {
+            Err(DatabaseError::UnexpectedChangeNumber {
+                expected: expected_number,
+                actual: log.serial_number,
+            })?
+        }
+
+        // apply the change
+        apply_change(&log.change).run(ctx)?;
+
+        // import the remote changelog entry
+        insert_new(&log.as_import_from(parent_node_id)).run(ctx)
+    })
+}
+
+fn apply_change<'a>(change: &'a Change) -> impl Operation<WritableConnection, ()> + 'a {
+    composite_op::<WritableConnection, _, _>(move |ctx| {
+        // TODO: implement it!
+        Ok(())
     })
 }
