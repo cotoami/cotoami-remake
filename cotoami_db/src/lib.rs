8e18e39abc92f92fda79f3cfb9127e6d9ab7cd38
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-06-01 15:34:07 +0900

    docs(db): add doc comments

--- cotoami_db/src/lib.rs
+++ cotoami_db/src/lib.rs
@@ -7,6 +7,8 @@ pub mod db;
 pub mod models;
 pub mod schema;
 
+/// Returns the current datetime in UTC.
+/// It is to recreate SQLite's `CURRENT_TIMESTAMP` in the Rust side.
 pub fn current_datetime() -> NaiveDateTime {
     Utc::now().naive_utc()
 }
