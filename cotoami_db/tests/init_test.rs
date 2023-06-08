f827c29a325a81c9a1e5d417e79866351a544648
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-06-08 21:43:40 +0900

    test(db): update init_test::init_as_node

--- cotoami_db/tests/init_test.rs
+++ cotoami_db/tests/init_test.rs
@@ -113,36 +113,51 @@ fn init_as_node() -> Result<()> {
     common::assert_approximately_now(&node.created_at());
     common::assert_approximately_now(&node.inserted_at());
 
+    let all_cotonomas = session.recent_cotonomas(None, 5, 0)?;
+    assert_eq!(all_cotonomas.rows.len(), 1);
+    assert_matches!(
+        all_cotonomas.rows[0],
+        Cotonoma {
+            node_id,
+            ref name,
+            ..
+        } if node_id == node.uuid &&
+             name == "My Node"
+    );
+    common::assert_approximately_now(&all_cotonomas.rows[0].created_at());
+    common::assert_approximately_now(&all_cotonomas.rows[0].updated_at());
+
+    let all_cotos = session.recent_cotos(None, None, 5, 0)?;
+    assert_eq!(all_cotos.rows.len(), 1);
+    assert_matches!(
+        all_cotos.rows[0],
+        Coto {
+            node_id,
+            posted_in_id: None,
+            posted_by_id,
+            content: None,
+            summary: Some(ref summary),
+            is_cotonoma: true,
+            repost_of_id: None,
+            reposted_in_ids: None,
+            ..
+        } if node_id == node.uuid &&
+             posted_by_id == node.uuid &&
+             summary == "My Node"
+    );
+    common::assert_approximately_now(&all_cotos.rows[0].created_at());
+    common::assert_approximately_now(&all_cotos.rows[0].updated_at());
+
     assert_matches!(
         changelog,
         ChangelogEntry {
             serial_number: 1,
             parent_node_id: None,
             parent_serial_number: None,
-            change: Change::CreateCotonoma(
-                Cotonoma {
-                    node_id: cotonoma_node_id,
-                    ref name,
-                    ..
-                },
-                Coto {
-                    node_id: coto_node_id,
-                    posted_in_id: None,
-                    posted_by_id,
-                    content: None,
-                    summary: Some(ref summary),
-                    is_cotonoma: true,
-                    repost_of_id: None,
-                    reposted_in_ids: None,
-                    ..
-                }
-            ),
+            change: Change::CreateCotonoma(cotonoma, coto),
             ..
-        } if cotonoma_node_id == node.uuid &&
-             name == "My Node" &&
-             coto_node_id == node.uuid &&
-             posted_by_id == node.uuid &&
-             summary == "My Node"
+        } if cotonoma == all_cotonomas.rows[0] &&
+             coto == all_cotos.rows[0]
     );
 
     Ok(())
