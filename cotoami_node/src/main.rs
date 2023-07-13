ebbcf3b8d72a8c85bb3fc0a6ec8e4bf4ba4d6d95
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-07-13 11:49:28 +0900

    feat(node): add a root path and fallback

--- cotoami_node/src/main.rs
+++ cotoami_node/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{http::Uri, response::IntoResponse, routing::get, Router, Server};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+const DEFAULT_PORT: u16 = 5103;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init(); // ex. export RUST_LOG=debug
+
+    let app = Router::new().fallback(fallback).route("/", get(root));
+
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), DEFAULT_PORT);
+    Server::bind(&socket)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback(uri: Uri) -> impl IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
