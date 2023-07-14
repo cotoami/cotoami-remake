1797561026e22e8b5880e214f0abc83b147a481b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-07-14 14:53:06 +0900

    feat(node): configuration via environment variables

--- cotoami_node/src/main.rs
+++ cotoami_node/src/main.rs
@@ -1,17 +1,20 @@
 use anyhow::Result;
 use axum::{http::Uri, response::IntoResponse, routing::get, Router, Server};
+use dotenvy::dotenv;
 use std::net::SocketAddr;
-
-const DEFAULT_PORT: u16 = 5103;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // install global collector configured based on RUST_LOG env var.
+    // Install global collector configured based on RUST_LOG env var.
     tracing_subscriber::fmt::init();
+
+    let config = Config::load()?;
+    info!("Config loaded: {:?}", config);
 
     let app = Router::new().fallback(fallback).route("/", get(root));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], DEFAULT_PORT));
+    let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
     Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -20,9 +23,30 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug, serde::Deserialize)]
+struct Config {
+    #[serde(default = "Config::default_port")]
+    port: u16,
+}
+
+impl Config {
+    const ENV_PREFXI: &str = "COTOAMI_";
+
+    fn load() -> Result<Config, envy::Error> {
+        dotenv().ok();
+        envy::prefixed(Self::ENV_PREFXI).from_env::<Config>()
+    }
+
+    // Functions returning a default value as a workaround for the issue:
+    // https://github.com/serde-rs/serde/issues/368
+    fn default_port() -> u16 {
+        5103
+    }
+}
+
 /// axum handler for any request that fails to match the router routes.
 /// This implementation returns HTTP status code Not Found (404).
-pub async fn fallback(uri: Uri) -> impl IntoResponse {
+async fn fallback(uri: Uri) -> impl IntoResponse {
     (
         axum::http::StatusCode::NOT_FOUND,
         format!("No route {}", uri),
