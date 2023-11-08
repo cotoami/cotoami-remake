ca60198bbbddfec2a19dad6f2061e988878ce64a
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-11-08 14:36:59 +0900

    refactor(node): delete http::nodes::routes

--- cotoami_node/src/http/nodes.rs
+++ cotoami_node/src/http/nodes.rs
@@ -1,19 +1,2 @@
-use axum::{extract::State, middleware, routing::get, Json, Router};
-
-use crate::{api::nodes, AppState};
-
 pub(crate) mod children;
 pub(crate) mod parents;
-
-pub(crate) fn routes() -> Router<AppState> {
-    Router::new()
-        .route(
-            "/local",
-            get(|State(state): State<AppState>| async {
-                nodes::local_node(state.db).await.map(Json)
-            }),
-        )
-        .nest("/parents", parents::routes())
-        .nest("/children", children::routes())
-        .layer(middleware::from_fn(super::require_session))
-}
