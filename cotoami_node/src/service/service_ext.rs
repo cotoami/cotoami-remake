334f6d57a9f0f7e191e47194e62c6a73e8b90ff0
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-11-21 17:19:46 +0900

    Fix compile errors in SseClient

--- cotoami_node/src/service/service_ext.rs
+++ cotoami_node/src/service/service_ext.rs
@@ -1,16 +1,10 @@
-use std::sync::Arc;
-
-use anyhow::{bail, Result};
+use anyhow::Result;
 use async_trait::async_trait;
-use cotoami_db::prelude::*;
-use tokio::task::spawn_blocking;
-use tracing::{debug, info};
 
 use crate::{
-    api,
     api::session::{ClientNodeSession, CreateClientNodeSession},
     service::{NodeService, RemoteNodeService, RequestBody},
-    state::{ChangePubsub, ChunkOfChanges},
+    state::ChunkOfChanges,
 };
 
 #[async_trait]
@@ -22,44 +16,6 @@ where
         let request = RequestBody::ChunkOfChanges { from }.into_request();
         let response = self.call(request).await?;
         response.message_pack::<ChunkOfChanges>()
-    }
-
-    async fn handle_parent_change(
-        &mut self,
-        parent_node_id: Id<Node>,
-        change: ChangelogEntry,
-        db: Arc<Database>,
-        change_pubsub: &Arc<ChangePubsub>,
-    ) -> Result<()> {
-        let import_result = spawn_blocking({
-            let db = db.clone();
-            move || db.new_session()?.import_change(&change, &parent_node_id)
-        })
-        .await?;
-        match import_result {
-            Err(anyhow_err) => {
-                if let Some(DatabaseError::UnexpectedChangeNumber {
-                    expected, actual, ..
-                }) = anyhow_err.downcast_ref::<DatabaseError>()
-                {
-                    info!(
-                        "Out of sync with {} (received: {}, expected {})",
-                        self.description(),
-                        actual,
-                        expected,
-                    );
-                    self.import_changes(parent_node_id, &db, change_pubsub)
-                        .await?;
-                } else {
-                    return Err(anyhow_err);
-                }
-            }
-            Ok(Some(imported_change)) => {
-                change_pubsub.publish(imported_change, None);
-            }
-            Ok(None) => (),
-        }
-        Ok(())
     }
 }
 
