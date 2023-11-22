2cc6a5c5433511fa06bf2d5c39c7871b67bba8d8
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-11-22 20:13:10 +0900

    feat(node): add web::events::post_event (uncompleted)

--- cotoami_node/src/state/impls/session.rs
+++ cotoami_node/src/state/impls/session.rs
@@ -4,7 +4,7 @@ use anyhow::{bail, Result};
 use tokio::task::spawn_blocking;
 
 use crate::{
-    service::models::{ClientNodeSession, CreateClientNodeSession, Session},
+    service::{ClientNodeSession, CreateClientNodeSession, Session},
     state::{error::NodeError, NodeState},
 };
 
--- cotoami_node/src/web/events.rs
+++ cotoami_node/src/web/events.rs
@@ -1,20 +1,25 @@
 use std::convert::Infallible;
 
 use axum::{
+    body::Bytes,
     extract::{Extension, State},
+    http::StatusCode,
     middleware,
-    response::sse::{Event, KeepAlive, Sse},
+    response::sse::{Event as SseEvent, KeepAlive, Sse},
     routing::get,
     Router,
 };
 use cotoami_db::prelude::*;
 use futures::stream::Stream;
 
-use crate::state::NodeState;
+use crate::{
+    service::{Event, ServiceError},
+    NodeState,
+};
 
 pub(super) fn routes() -> Router<NodeState> {
     Router::new()
-        .route("/", get(stream_events))
+        .route("/", get(stream_events).post(post_event))
         .layer(middleware::from_fn(super::require_session))
 }
 
@@ -25,8 +30,34 @@ pub(super) fn routes() -> Router<NodeState> {
 async fn stream_events(
     State(state): State<NodeState>,
     Extension(_session): Extension<ClientSession>,
-) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+) -> Sse<impl Stream<Item = Result<SseEvent, Infallible>>> {
     // FIXME: subscribe to changes or requests
     let sub = state.pubsub().sse_change.subscribe(None::<()>);
     Sse::new(sub).keep_alive(KeepAlive::default())
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// POST /api/events
+/////////////////////////////////////////////////////////////////////////////
+
+async fn post_event(
+    State(state): State<NodeState>,
+    Extension(session): Extension<ClientSession>,
+    body: Bytes,
+) -> Result<StatusCode, ServiceError> {
+    if let ClientSession::ParentNode(parent) = session {
+        let event: Event = rmp_serde::from_slice(&body)?;
+
+        // TODO:
+        // state
+        //     .handle_event(parent.node_id, event, source_service)
+        //     .await?;
+
+        // it won't create an "event" resouce, just handle it,
+        // so returns `OK` instead of `Created`q
+        Ok(StatusCode::OK)
+    } else {
+        // Only a parent can send an event
+        Err(ServiceError::Permission)
+    }
 }
