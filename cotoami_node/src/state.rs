9feb919bb232d88a4dba9a9423c3d5fc38a71914
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-11-22 10:08:49 +0900

    Move Pubsub to a pubsub submodule

--- cotoami_node/src/state.rs
+++ cotoami_node/src/state.rs
@@ -1,14 +1,12 @@
-use std::{convert::Infallible, fs, sync::Arc};
+use std::{fs, sync::Arc};
 
 use anyhow::{anyhow, Result};
-use axum::response::sse::Event;
 use cotoami_db::prelude::*;
 use parking_lot::{MappedRwLockReadGuard, RwLock, RwLockReadGuard};
 use tokio::task::spawn_blocking;
 use validator::Validate;
 
 use self::conn::{ServerConnection, ServerConnections};
-use crate::pubsub::Publisher;
 
 mod changes;
 mod config;
@@ -16,14 +14,11 @@ pub(crate) mod conn;
 mod error;
 mod nodes;
 mod parents;
+mod pubsub;
 mod service;
 mod session;
 
-pub use self::config::Config;
-
-/////////////////////////////////////////////////////////////////////////////
-// NodeState
-/////////////////////////////////////////////////////////////////////////////
+pub use self::{config::Config, pubsub::*};
 
 #[derive(Clone)]
 pub struct NodeState {
@@ -102,41 +97,3 @@ impl NodeState {
         Ok(())
     }
 }
-
-/////////////////////////////////////////////////////////////////////////////
-// Pubsub
-/////////////////////////////////////////////////////////////////////////////
-
-#[derive(Clone)]
-pub struct Pubsub {
-    pub local_change: Arc<ChangePubsub>,
-    pub sse_change: Arc<SsePubsub>,
-}
-
-impl Pubsub {
-    fn new() -> Self {
-        let local_change = ChangePubsub::new();
-        let sse_change = SsePubsub::new();
-
-        sse_change.tap_into(
-            local_change.subscribe(None::<()>),
-            |_| None,
-            |change| {
-                let event = Event::default().event("change").json_data(change)?;
-                Ok(Ok(event))
-            },
-        );
-
-        Self {
-            local_change: Arc::new(local_change),
-            sse_change: Arc::new(sse_change),
-        }
-    }
-
-    pub fn publish_change(&self, changelog: ChangelogEntry) {
-        self.local_change.publish(changelog, None);
-    }
-}
-
-pub(crate) type ChangePubsub = Publisher<ChangelogEntry, ()>;
-pub(crate) type SsePubsub = Publisher<Result<Event, Infallible>, ()>;
