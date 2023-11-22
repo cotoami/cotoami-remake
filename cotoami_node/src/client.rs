57d24c058b81d0cbc7f773bd6e7b8260355a0483
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-11-22 12:14:27 +0900

    Minor refactoring

--- cotoami_node/src/client.rs
+++ cotoami_node/src/client.rs
@@ -1,5 +1,7 @@
 mod http_client;
 mod sse_client;
 
-pub use http_client::HttpClient;
-pub use sse_client::{SseClient, SseClientError, SseClientState};
+pub use self::{
+    http_client::HttpClient,
+    sse_client::{SseClient, SseClientError, SseClientState},
+};
