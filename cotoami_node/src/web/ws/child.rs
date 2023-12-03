1aaaeb3c5dfa7c3da7c330fa0e0bf964de280c4b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-12-04 08:58:03 +0900

    Add handle_event

--- cotoami_node/src/web/ws/child.rs
+++ cotoami_node/src/web/ws/child.rs
@@ -1,7 +1,7 @@
-use axum::extract::ws::WebSocket;
+use axum::extract::ws::{Message, WebSocket};
 use cotoami_db::prelude::*;
 use futures::StreamExt;
-use tracing::{debug, error};
+use tracing::{debug, error, info};
 
 use crate::{event::NodeSentEvent, state::NodeState};
 
@@ -9,19 +9,37 @@ pub(super) async fn handle_child(socket: WebSocket, state: NodeState, operator: 
     let (mut sender, mut receiver) = socket.split();
 
     // Publish change events
+    let node_id = operator.node_id();
     let mut changes = state.pubsub().local_changes().subscribe(None::<()>);
     let mut send_task = tokio::spawn(async move {
         while let Some(change) = changes.next().await {
             if let Err(e) = super::send_event(&mut sender, NodeSentEvent::Change(change)).await {
-                debug!("Client ({}) disconnected: {e}", operator.node_id());
+                debug!("Client ({}) disconnected: {e}", node_id);
             }
         }
     });
 
     // Accept request events
     let mut recv_task = tokio::spawn(async move {
-        while let Some(Ok(message)) = receiver.next().await {
-            //
+        while let Some(Ok(msg)) = receiver.next().await {
+            match msg {
+                Message::Binary(vec) => match rmp_serde::from_slice::<NodeSentEvent>(&vec) {
+                    Ok(event) => {
+                        handle_event(event, state.clone(), operator.clone()).await;
+                    }
+                    Err(e) => {
+                        // A malicious　client can send invalid message intentionally,
+                        // so let's not handle it as errors.
+                        info!("Child ({node_id}) sent an invalid binary message: {e}");
+                        return;
+                    }
+                },
+                Message::Close(c) => {
+                    info!("Child ({node_id}) sent close with: {c:?}");
+                    return;
+                }
+                _ => debug!(""),
+            }
         }
     });
 
@@ -40,4 +58,8 @@ pub(super) async fn handle_child(socket: WebSocket, state: NodeState, operator: 
             send_task.abort();
         }
     }
+}
+
+async fn handle_event(event: NodeSentEvent, state: NodeState, operator: Operator) {
+    //
 }
