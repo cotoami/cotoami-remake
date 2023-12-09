8df7934765a8834a02187c98097122ba207b431f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-12-09 15:40:46 +0900

    Update client::ws

--- cotoami_node/src/client/ws.rs
+++ cotoami_node/src/client/ws.rs
@@ -1,7 +1,17 @@
 //! WebSocket client of Node API Service.
 
-use tokio_tungstenite::WebSocketStream;
+use anyhow::Result;
+use tokio_tungstenite::{connect_async, WebSocketStream};
+use url::Url;
 
-pub struct WebSocket {
-    //
+pub struct WebSocketClient {
+    url_prefix: String,
+}
+
+impl WebSocketClient {
+    pub async fn new(url_prefix: String) -> Result<Self> {
+        let ws_url = Url::parse(&url_prefix)?.join("/api/ws")?;
+        let (ws_stream, _) = connect_async(ws_url).await?;
+        Ok(Self { url_prefix })
+    }
 }
