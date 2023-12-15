a5ed9548e7a519210dae2d0f323e11c31eddfc49
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-12-15 17:54:23 +0900

    Add WebSocketClient

--- cotoami_node/src/client/ws.rs
+++ cotoami_node/src/client/ws.rs
@@ -1,17 +1,33 @@
 //! WebSocket client of Node API Service.
 
+use std::sync::Arc;
+
 use anyhow::Result;
+use cotoami_db::{ChangelogEntry, Id, Node, Operator};
 use tokio_tungstenite::{connect_async, WebSocketStream};
 use url::Url;
 
+#[derive(Clone)]
 pub struct WebSocketClient {
+    state: Arc<State>,
+}
+
+struct State {
+    server_id: Id<Node>,
     url_prefix: String,
+    ws_url: Url,
 }
 
 impl WebSocketClient {
-    pub async fn new(url_prefix: String) -> Result<Self> {
+    pub fn new(server_id: Id<Node>, url_prefix: String) -> Result<Self> {
         let ws_url = Url::parse(&url_prefix)?.join("/api/ws")?;
-        let (ws_stream, _) = connect_async(ws_url).await?;
-        Ok(Self { url_prefix })
+        let state = State {
+            server_id,
+            url_prefix,
+            ws_url,
+        };
+        Ok(Self {
+            state: Arc::new(state),
+        })
     }
 }
