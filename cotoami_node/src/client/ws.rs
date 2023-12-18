c90c68c23a1ba1c0d3430c0b5fe80c9df56a9a89
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-12-18 10:37:02 +0900

    Add WebSocketClient::send_event

--- cotoami_node/src/client/ws.rs
+++ cotoami_node/src/client/ws.rs
@@ -3,13 +3,15 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use bytes::Bytes;
 use cotoami_db::{Id, Node};
-use futures::StreamExt;
-use tokio_tungstenite::connect_async;
+use futures::{Sink, SinkExt, StreamExt};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use url::Url;
 
 use crate::{
     client::{ClientState, ConnectionState},
+    event::NodeSentEvent,
     service::models::NotConnected,
     state::NodeState,
 };
@@ -52,4 +54,14 @@ impl WebSocketClient {
     }
 
     pub fn disconnect(&mut self) { self.state.disconnect(); }
+
+    async fn send_event<S, E>(mut message_sink: S, event: NodeSentEvent) -> Result<(), E>
+    where
+        S: Sink<Message, Error = E> + Unpin,
+    {
+        let bytes = rmp_serde::to_vec(&event)
+            .map(Bytes::from)
+            .expect("A NodeSentEvent should be serializable into MessagePack");
+        message_sink.send(Message::Binary(bytes.into())).await
+    }
 }
