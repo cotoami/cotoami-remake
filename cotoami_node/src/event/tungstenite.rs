68c1219239e71efc7015d0a4234f67c4748af706
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-12-18 14:04:13 +0900

    Add event::tungstenite::handle_message_stream

--- cotoami_node/src/event/tungstenite.rs
+++ cotoami_node/src/event/tungstenite.rs
@@ -1,8 +1,43 @@
+use std::{future::Future, marker::Unpin, ops::ControlFlow};
+
 use bytes::Bytes;
-use futures::{Sink, SinkExt};
+use cotoami_db::{Id, Node};
+use futures::{Sink, SinkExt, Stream, StreamExt};
 use tokio_tungstenite::tungstenite::protocol::Message;
+use tracing::{debug, info};
 
 use crate::event::NodeSentEvent;
+
+pub(crate) async fn handle_message_stream<S, E, H, F>(mut stream: S, peer_id: Id<Node>, handler: H)
+where
+    S: Stream<Item = Result<Message, E>> + Unpin,
+    E: Into<anyhow::Error>,
+    H: Fn(NodeSentEvent) -> F,
+    F: Future<Output = ControlFlow<anyhow::Error>>,
+{
+    while let Some(Ok(msg)) = stream.next().await {
+        match msg {
+            Message::Binary(vec) => match rmp_serde::from_slice::<NodeSentEvent>(&vec) {
+                Ok(event) => {
+                    if handler(event).await.is_break() {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    // A malicious client can send an invalid message intentionally,
+                    // so let's not handle it as an error.
+                    info!("The peer ({peer_id}) sent an invalid binary message: {e}");
+                    break;
+                }
+            },
+            Message::Close(c) => {
+                info!("The peer ({peer_id}) sent close with: {c:?}");
+                break;
+            }
+            the_others => debug!("Message ignored: {:?}", the_others),
+        }
+    }
+}
 
 pub(crate) async fn send_event<S, E>(mut message_sink: S, event: NodeSentEvent) -> Result<(), E>
 where
