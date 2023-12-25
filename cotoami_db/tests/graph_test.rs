f91c186de1673c121dd1831d4a6eeaf389dbdc86
Daisuke Morita <daisuke.marubinotto@gmail.com>
2023-12-25 20:17:20 +0900

    Update graph_test

--- cotoami_db/tests/graph_test.rs
+++ cotoami_db/tests/graph_test.rs
@@ -11,34 +11,59 @@ fn graph() -> Result<()> {
     /////////////////////////////////////////////////////////////////////////////
     let (_root_dir, db, _node) = common::setup_db("My Node")?;
     let mut ds = db.new_session()?;
-    let operator = db.globals().local_node_as_operator()?;
-    let (root_cotonoma, _) = ds.root_cotonoma()?.unwrap();
+    let opr = db.globals().local_node_as_operator()?;
+    let (root, _) = ds.root_cotonoma()?.unwrap();
 
     /////////////////////////////////////////////////////////////////////////////
-    // When: single coto
+    // When: add a child
     /////////////////////////////////////////////////////////////////////////////
-    let (coto, _) = ds.post_coto("hello", None, &root_cotonoma, &operator)?;
-    let _ = ds.create_link(
-        &root_cotonoma.coto_id,
-        &coto.uuid,
-        Some("foo"),
-        None,
-        Some(&root_cotonoma),
-        &operator,
-    )?;
+
+    let (coto1, _) = ds.post_coto("coto1", None, &root, &opr)?;
+    let _ = ds.create_link(&root.coto_id, &coto1.uuid, Some("foo"), None, None, &opr)?;
 
     /////////////////////////////////////////////////////////////////////////////
     // Then
     /////////////////////////////////////////////////////////////////////////////
 
-    let graph = ds.graph(root_cotonoma, true)?;
+    let graph = ds.graph(root.clone(), true)?;
     assert_eq!(
         Dot::new(&graph.into_petgraph()).to_string(),
         indoc! {r#"
             digraph {
                 0 [ label = "<My Node>" ]
-                1 [ label = "hello" ]
+                1 [ label = "coto1" ]
                 0 -> 1 [ label = "foo" ]
+            }
+            "#, 
+        }
+    );
+
+    /////////////////////////////////////////////////////////////////////////////
+    // When: add grandchildren
+    /////////////////////////////////////////////////////////////////////////////
+
+    let (coto2, _) = ds.post_coto("coto2", None, &root, &opr)?;
+    let _ = ds.create_link(&coto1.uuid, &coto2.uuid, None, None, None, &opr)?;
+
+    let ((cotonoma1, _), _) = ds.post_cotonoma("cotonoma1", &root, &opr)?;
+    let _ = ds.create_link(&coto1.uuid, &cotonoma1.coto_id, None, None, None, &opr)?;
+
+    /////////////////////////////////////////////////////////////////////////////
+    // Then
+    /////////////////////////////////////////////////////////////////////////////
+
+    let graph = ds.graph(root.clone(), true)?;
+    assert_eq!(
+        Dot::new(&graph.into_petgraph()).to_string(),
+        indoc! {r#"
+            digraph {
+                0 [ label = "<My Node>" ]
+                1 [ label = "coto1" ]
+                2 [ label = "coto2" ]
+                3 [ label = "<cotonoma1>" ]
+                0 -> 1 [ label = "foo" ]
+                1 -> 2 [ label = "" ]
+                1 -> 3 [ label = "" ]
             }
             "#, 
         }
