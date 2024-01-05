407b644b1aed54cc0719bae40df00815176d31fe
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-01-05 14:40:56 +0900

    test(db): improve search_test.rs

--- cotoami_db/tests/search_test.rs
+++ cotoami_db/tests/search_test.rs
@@ -12,33 +12,69 @@ fn search_cotos() -> Result<()> {
 
     // when
     let (coto1, _) = ds.post_coto("Hello, world!", None, &root, &opr)?;
-    let (coto2, _) = ds.post_coto("It's a small world.", None, &root, &opr)?;
+    let (coto2, _) = ds.post_coto("It's a small world.", Some("summary"), &root, &opr)?;
     let (coto3, _) = ds.post_coto("柿くへば鐘が鳴るなり法隆寺", None, &root, &opr)?;
-    let coto1_id = coto1.uuid;
-    let coto2_id = coto2.uuid;
 
     // then
-    assert_eq!(ds.search_cotos("hello")?, vec![coto1.clone()]);
-    assert_eq!(ds.search_cotos("world")?, vec![coto1, coto2]);
-    assert_eq!(ds.search_cotos("法隆寺")?, vec![coto3.clone()]);
+    assert_eq!(
+        ds.search_cotos("hello")?.iter().collect::<Vec<_>>(),
+        vec![&coto1]
+    );
+    assert_eq!(
+        ds.search_cotos("world")?.iter().collect::<Vec<_>>(),
+        vec![&coto1, &coto2]
+    );
+    assert_eq!(
+        ds.search_cotos("法隆寺")?.iter().collect::<Vec<_>>(),
+        vec![&coto3]
+    );
+    assert_eq!(
+        ds.search_cotos("summary")?.iter().collect::<Vec<_>>(),
+        vec![&coto2]
+    );
+    assert_eq!(
+        ds.search_cotos("small OR world")?
+            .iter()
+            .collect::<Vec<_>>(),
+        vec![&coto2, &coto1]
+    );
+    assert_eq!(
+        ds.search_cotos("small AND world")?
+            .iter()
+            .collect::<Vec<_>>(),
+        vec![&coto2]
+    );
 
-    // when: delete a coto
-    let _ = ds.delete_coto(&coto1_id, &opr)?;
+    // when: edit a coto to change English results
+    // (testing the trigger: `cotos_fts_update`)
+    let (coto2, _) = ds.edit_coto(&coto2.uuid, "No Promises to Keep", None, &opr)?;
+    assert_eq!(
+        ds.search_cotos("world")?.iter().collect::<Vec<_>>(),
+        vec![&coto1]
+    );
+    assert_eq!(
+        ds.search_cotos("promise")?.iter().collect::<Vec<_>>(),
+        vec![&coto2]
+    );
 
-    // then
+    // when: edit a coto to change CJK results
+    // (testing the trigger: `cotos_fts_update`)
+    let (coto3, _) = ds.edit_coto(&coto3.uuid, "色即是空空即是色", None, &opr)?;
+    assert_eq!(ds.search_cotos("法隆寺")?, vec![]);
+    assert_eq!(
+        ds.search_cotos("色即是空")?.iter().collect::<Vec<_>>(),
+        vec![&coto3]
+    );
+
+    // when: delete a coto in English
+    // (testing the trigger: `cotos_fts_delete`)
+    let _ = ds.delete_coto(&coto1.uuid, &opr)?;
     assert_eq!(ds.search_cotos("hello")?, vec![]);
 
-    // when: edit a coto to add a "法隆寺" result
-    let (coto2, _) = ds.edit_coto(&coto2_id, "法隆寺", None, &opr)?;
-
-    // then
-    assert_eq!(ds.search_cotos("法隆寺")?, vec![coto2, coto3.clone()]);
-
-    // when: edit a coto to remove a "法隆寺" result
-    let (_coto2, _) = ds.edit_coto(&coto2_id, "斑鳩寺", None, &opr)?;
-
-    // then
-    assert_eq!(ds.search_cotos("法隆寺")?, vec![coto3]);
+    // when: delete a coto in CJK
+    // (testing the trigger: `cotos_fts_delete`)
+    let _ = ds.delete_coto(&coto3.uuid, &opr)?;
+    assert_eq!(ds.search_cotos("色即是空")?, vec![]);
 
     Ok(())
 }
