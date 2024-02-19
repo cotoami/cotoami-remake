e8f64dd2cc23515bc8f484d3cb94f46d4a08fc6b
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-02-19 21:38:39 +0900

    Add drag and drop handling

--- cotoami_desktop/ui/src/main/scala/cotoami/SplitPane.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/SplitPane.scala
@@ -1,5 +1,7 @@
 package cotoami
 
+import scala.scalajs.js
+import org.scalajs.dom
 import org.scalajs.dom.html
 
 import slinky.core._
@@ -7,6 +9,7 @@ import slinky.core.annotations.react
 import slinky.core.facade.{React, ReactElement, SetStateHookCallback}
 import slinky.core.facade.Hooks._
 import slinky.web.html._
+import slinky.web.SyntheticMouseEvent
 
 @react object SplitPane {
   case class Props(
@@ -25,13 +28,64 @@ import slinky.web.html._
 
   val component = FunctionalComponent[Props] { props =>
     val (primarySize, setPrimarySize) = useState(props.initialPrimarySize)
+    val splitPaneRef = React.createRef[html.Div]
+    val separatorPosition = useRef(Double.NaN)
 
-    div(className := s"split-pane ${props.split} ${props.className}")(
+    val onMouseDownOnSeparator =
+      (e: SyntheticMouseEvent[dom.HTMLDivElement]) => {
+        props.split match {
+          case "vertical" =>
+            separatorPosition.current = e.clientX
+          case "horizontal" =>
+            separatorPosition.current = e.clientY
+        }
+      }
+
+    // Define the following event listeners as js.Function1 to avoid implicit
+    // conversion from scala.Function1 to js.Function1 when passing them to
+    // addEventListener and removeEventListener, which won't work as expected
+    // because they become different references as a result of conversion.
+
+    val onMouseMove: js.Function1[dom.MouseEvent, Unit] =
+      (e: dom.MouseEvent) => {
+        if (!separatorPosition.current.isNaN()) {
+          val pointer = props.split match {
+            case "vertical"   => e.clientX
+            case "horizontal" => e.clientY
+          }
+          val moved = (pointer - separatorPosition.current).toInt
+          val newSize = primarySize + moved
+          separatorPosition.current = pointer
+          setPrimarySize(newSize)
+        }
+      }
+
+    val onMouseUp: js.Function1[dom.MouseEvent, Unit] = (e: dom.MouseEvent) => {
+      separatorPosition.current = Double.NaN
+    }
+
+    useEffect(() => {
+      dom.document.addEventListener("mousemove", onMouseMove)
+      dom.document.addEventListener("mouseup", onMouseUp)
+
+      () => {
+        dom.document.removeEventListener("mousemove", onMouseMove)
+        dom.document.removeEventListener("mouseup", onMouseUp)
+      }
+    })
+
+    div(
+      className := s"split-pane ${props.split} ${props.className}",
+      ref := splitPaneRef
+    )(
       splitPaneContext.Provider(value =
         Context(props.split, primarySize, setPrimarySize)
       )(
         props.children(0),
-        div(className := "separator"),
+        div(
+          className := "separator",
+          onMouseDown := (onMouseDownOnSeparator(_))
+        ),
         props.children(1)
       )
     )
