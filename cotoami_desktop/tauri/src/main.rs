622fac5fc11499598803709047c07707cdd1d052
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-02-24 00:28:56 +0900

    fix(desktop): add a workaround for a webview resizing bug on macOS

--- cotoami_desktop/tauri/src/main.rs
+++ cotoami_desktop/tauri/src/main.rs
@@ -3,9 +3,11 @@
     windows_subsystem = "windows"
 )]
 
+pub mod window_state;
+
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
