53ce900b857d94ecff43d9b6391da58bfbefc24f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-02-27 16:17:16 +0900

    feat(desktop): add cotoami.tauri.Event

---
+++ cotoami_desktop/ui/src/main/scala/cotoami/tauri/Event.scala
@@ -0,0 +1,61 @@
+package cotoami.tauri
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+/** The event system allows you to emit events to the backend and listen to
+  * events from it.
+  */
+@js.native
+@JSImport("@tauri-apps/api/event", JSImport.Namespace)
+object Event extends js.Object {
+
+  /** Emits an event to the backend and all Tauri windows.
+    *
+    * @param event
+    *   Event name. Must include only alphanumeric characters, -, /, : and _.
+    * @param payload
+    *   Object payload.
+    * @return
+    */
+  def emit(event: String, payload: js.Object): js.Promise[Unit] = js.native
+
+  /** Listen to an event. The event can be either global or window-specific. See
+    * windowLabel to check the event source.
+    *
+    * @param event
+    *   Event name. Must include only alphanumeric characters, -, /, : and _.
+    * @param handler
+    *   Event handler callback.
+    * @return
+    *   A promise resolving to a function to unlisten to the event. Note that
+    *   removing the listener is required if your listener goes out of scope
+    *   e.g. the component is unmounted.
+    */
+  def listen[T](
+      event: String,
+      handler: js.Function1[Event[T], Unit]
+  ): js.Promise[js.Function0[Unit]] = js.native
+
+  /** Listen to an one-off event. See listen for more information.
+    */
+  def once[T](
+      event: String,
+      handler: js.Function1[Event[T], Unit]
+  ): js.Promise[js.Function0[Unit]] = js.native
+}
+
+@js.native
+trait Event[T] extends js.Object {
+  // Event name
+  val event: String = js.native
+
+  // Event identifier used to unlisten
+  val id: Int = js.native
+
+  // Event payload
+  val payload: T = js.native
+
+  // The label of the window that emitted this event
+  val windowLabel: String = js.native
+}
