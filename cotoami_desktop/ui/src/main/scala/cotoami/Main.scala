9930a58fd2b6e567dbe4d07b472137432a23d462
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-03-28 18:05:54 +0900

    refactor(desktop): add a package object: cotoami.subparts

--- cotoami_desktop/ui/src/main/scala/cotoami/Main.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/Main.scala
@@ -14,7 +14,6 @@ import cats.effect.IO
 
 import fui.FunctionalUI._
 import cotoami.tauri
-import cotoami.components.{material_symbol, node_img, paneToggle, SplitPane}
 import cotoami.backend.{
   Commands,
   DatabaseInfo,
@@ -233,113 +232,17 @@ object Main {
 
   def view(model: Model, dispatch: Msg => Unit): ReactElement =
     Fragment(
-      appHeader(model, dispatch),
+      subparts.appHeader(model, dispatch),
       div(id := "app-body", className := "body")(
         model.uiState
-          .map(appBodyContent(model, _, dispatch))
+          .map(subparts.appBodyContent(model, _, dispatch))
           .getOrElse(Seq()): _*
       ),
-      appFooter(model, dispatch),
+      subparts.appFooter(model, dispatch),
       if (model.logViewToggle)
         Some(subparts.LogView.view(model.log, dispatch))
       else
         None,
-      modal(model, dispatch)
+      subparts.modal(model, dispatch)
     )
-
-  def appHeader(model: Model, dispatch: Msg => Unit): ReactElement =
-    header(
-      button(
-        className := "app-info default",
-        title := "View app info"
-      )(
-        img(
-          className := "app-icon",
-          alt := "Cotoami",
-          src := "/images/logo/logomark.svg"
-        )
-      ),
-      model
-        .nodes
-        .current
-        .map(node =>
-          Fragment(
-            section(className := "location")(
-              a(className := "node-home", title := node.name)(node_img(node))
-            ),
-            model.cotonomas.selected.map(cotonoma =>
-              Fragment(
-                material_symbol("chevron_right"),
-                h1(className := "current-cotonoma")(cotonoma.name)
-              )
-            )
-          )
-        )
-    )
-
-  def appBodyContent(
-      model: Model,
-      uiState: Model.UiState,
-      dispatch: Msg => Unit
-  ): Seq[ReactElement] = Seq(
-    subparts.NavNodes.view(model, uiState, dispatch),
-    SplitPane(
-      vertical = true,
-      initialPrimarySize = uiState.paneSizes.getOrElse(
-        subparts.NavCotonomas.PaneName,
-        subparts.NavCotonomas.DefaultWidth
-      ),
-      resizable = uiState.paneOpened(subparts.NavCotonomas.PaneName),
-      className = Some("node-contents"),
-      onPrimarySizeChanged = (
-          (newSize) =>
-            dispatch(ResizePane(subparts.NavCotonomas.PaneName, newSize))
-      )
-    )(
-      subparts.NavCotonomas.view(model, uiState, dispatch),
-      components.SplitPane.Secondary(className = None)(
-        slinky.web.html.main()(
-          section(className := "flow pane")(
-            paneToggle("flow", dispatch),
-            section(className := "timeline header-and-body")(
-            )
-          )
-        )
-      )
-    )
-  )
-
-  def appFooter(model: Model, dispatch: Msg => Unit): ReactElement =
-    footer(
-      div(className := "browser-nav")(
-        div(className := "path")(model.path)
-      ),
-      model.log
-        .lastEntry()
-        .map(entry =>
-          div(className := s"log-peek ${entry.level.name}")(
-            button(
-              className := "open-log-view default",
-              onClick := ((e) => dispatch(cotoami.ToggleLogView))
-            )(
-              material_symbol(entry.level.icon),
-              entry.message
-            )
-          )
-        )
-    )
-
-  def modal(model: Model, dispatch: Msg => Unit): Option[ReactElement] =
-    if (model.nodes.local.isEmpty) {
-      model.systemInfo.map(info =>
-        subparts.ModalWelcome
-          .view(
-            model.modalWelcome,
-            info.recent_databases.toSeq,
-            dispatch
-          )
-      )
-    } else {
-      None
-    }
 }
---
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/package.scala
@@ -0,0 +1,105 @@
+package cotoami
+
+import slinky.core.facade.{Fragment, ReactElement}
+import slinky.web.html._
+
+import cotoami.components.{material_symbol, node_img, paneToggle, SplitPane}
+
+package object subparts {
+
+  def appHeader(model: Model, dispatch: Msg => Unit): ReactElement =
+    header(
+      button(
+        className := "app-info default",
+        title := "View app info"
+      )(
+        img(
+          className := "app-icon",
+          alt := "Cotoami",
+          src := "/images/logo/logomark.svg"
+        )
+      ),
+      model
+        .nodes
+        .current
+        .map(node =>
+          Fragment(
+            section(className := "location")(
+              a(className := "node-home", title := node.name)(node_img(node))
+            ),
+            model.cotonomas.selected.map(cotonoma =>
+              Fragment(
+                material_symbol("chevron_right"),
+                h1(className := "current-cotonoma")(cotonoma.name)
+              )
+            )
+          )
+        )
+    )
+
+  def appBodyContent(
+      model: Model,
+      uiState: Model.UiState,
+      dispatch: Msg => Unit
+  ): Seq[ReactElement] = Seq(
+    subparts.NavNodes.view(model, uiState, dispatch),
+    SplitPane(
+      vertical = true,
+      initialPrimarySize = uiState.paneSizes.getOrElse(
+        subparts.NavCotonomas.PaneName,
+        subparts.NavCotonomas.DefaultWidth
+      ),
+      resizable = uiState.paneOpened(subparts.NavCotonomas.PaneName),
+      className = Some("node-contents"),
+      onPrimarySizeChanged = (
+          (newSize) =>
+            dispatch(ResizePane(subparts.NavCotonomas.PaneName, newSize))
+      )
+    )(
+      subparts.NavCotonomas.view(model, uiState, dispatch),
+      components.SplitPane.Secondary(className = None)(
+        slinky.web.html.main()(
+          section(className := "flow pane")(
+            paneToggle("flow", dispatch),
+            section(className := "timeline header-and-body")(
+            )
+          )
+        )
+      )
+    )
+  )
+
+  def appFooter(model: Model, dispatch: Msg => Unit): ReactElement =
+    footer(
+      div(className := "browser-nav")(
+        div(className := "path")(model.path)
+      ),
+      model.log
+        .lastEntry()
+        .map(entry =>
+          div(className := s"log-peek ${entry.level.name}")(
+            button(
+              className := "open-log-view default",
+              onClick := ((e) => dispatch(cotoami.ToggleLogView))
+            )(
+              material_symbol(entry.level.icon),
+              entry.message
+            )
+          )
+        )
+    )
+
+  def modal(model: Model, dispatch: Msg => Unit): Option[ReactElement] =
+    if (model.nodes.local.isEmpty) {
+      model.systemInfo.map(info =>
+        ModalWelcome
+          .view(
+            model.modalWelcome,
+            info.recent_databases.toSeq,
+            dispatch
+          )
+      )
+    } else {
+      None
+    }
+}
