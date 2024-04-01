ad4a828f9be03b2b437d72625cb3dd25729ebbd6
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-04-01 19:15:12 +0900

    Update CotoInput

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/CotoInput.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/CotoInput.scala
@@ -4,6 +4,7 @@ import slinky.core.facade.ReactElement
 import slinky.web.html._
 
 import cotoami.backend.{Cotonoma, Node}
+import cotoami.components.{material_symbol, node_img}
 
 object CotoInput {
 
@@ -19,5 +20,43 @@ object CotoInput {
       currentCotonoma: Cotonoma,
       dispatch: cotoami.Msg => Unit
   ): ReactElement =
-    section(className := "coto-input")("hello")
+    section(className := "coto-input")(
+      header(className := "tools")(
+        section(className := "coto-type-switch")(
+          button(className := "new-coto default", disabled := true)(
+            material_symbol("text_snippet"),
+            "Coto"
+          ),
+          button(className := "new-cotonoma default", disabled := false)(
+            material_symbol("topic"),
+            "Cotonoma"
+          )
+        ),
+        button(
+          className := "image default",
+          data - "tooltip" := "Image",
+          data - "placement" := "bottom"
+        )(
+          material_symbol("image")
+        ),
+        button(
+          className := "location default",
+          data - "tooltip" := "Location",
+          data - "placement" := "bottom"
+        )(
+          material_symbol("location_on")
+        )
+      ),
+      textarea(placeholder := "Write your Coto in Markdown here")(),
+      footer(className := "post")(
+        address(className := "poster")(
+          node_img(operatingNode),
+          operatingNode.name
+        ),
+        button(className := "post", disabled := true)(
+          s"Post to \"${currentCotonoma.name}\"",
+          span(className := "shortcut-help")("(Ctrl + Enter)")
+        )
+      )
+    )
 }
