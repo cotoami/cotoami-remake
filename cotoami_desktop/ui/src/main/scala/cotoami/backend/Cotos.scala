56084c653728f1997993955b6f64312efe3a0917
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-04-04 14:15:50 +0900

    Add CotosJson

--- cotoami_desktop/ui/src/main/scala/cotoami/backend/Cotos.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/backend/Cotos.scala
@@ -1,23 +1,26 @@
 package cotoami.backend
 
+import scala.scalajs.js
 import cotoami.Id
 
 case class Cotos(
     map: Map[Id[Coto], Coto] = Map.empty,
 
-    // Recent
-    recentIds: PaginatedIds[Coto] = PaginatedIds(),
-    recentLoading: Boolean = false
+    // Timeline
+    timelineIds: PaginatedIds[Coto] = PaginatedIds(),
+    timelineLoading: Boolean = false
 ) {
   def get(id: Id[Coto]): Option[Coto] = this.map.get(id)
 
-  def recent: Seq[Coto] = this.recentIds.order.map(this.get).flatten
+  def timeline: Seq[Coto] = this.timelineIds.order.map(this.get).flatten
 
-  def addPageOfRecent(page: Paginated[CotoJson]): Cotos =
+  def appendTimeline(cotos: CotosJson): Cotos =
     this.copy(
-      map = this.map ++ Coto.toMap(page.rows),
-      recentIds = this.recentIds.addPage(
-        page,
+      map = this.map ++
+        Coto.toMap(cotos.paginated.rows) ++
+        Coto.toMap(cotos.repost_of),
+      timelineIds = this.timelineIds.addPage(
+        cotos.paginated,
         (json: CotoJson) => Id[Coto](json.uuid)
       )
     )
@@ -26,8 +29,12 @@ case class Cotos(
 object Cotos {
   sealed trait Msg
 
-  case object FetchMoreRecent extends Msg
-  case class RecentFetched(
-      result: Either[Error, Paginated[CotoJson]]
-  ) extends Msg
+  case object FetchMoreTimeline extends Msg
+}
+
+@js.native
+trait CotosJson extends js.Object {
+  val paginated: Paginated[CotoJson] = js.native
+  val posted_in: js.Array[CotonomaJson] = js.native
+  val repost_of: js.Array[CotoJson] = js.native
 }
