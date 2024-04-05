3c2557a2361445d4411577d6430b3e0ccc47bedd
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-04-05 11:23:12 +0900

    refactor(desktop): extract cotoami.backend.Nodes as a single file

---
+++ cotoami_desktop/ui/src/main/scala/cotoami/backend/Nodes.scala
@@ -0,0 +1,46 @@
+package cotoami.backend
+
+import cotoami.Id
+
+case class Nodes(
+    map: Map[Id[Node], Node] = Map.empty,
+    localId: Option[Id[Node]] = None,
+    operatingId: Option[Id[Node]] = None,
+    parentIds: Seq[Id[Node]] = Seq.empty,
+    selectedId: Option[Id[Node]] = None
+) {
+  def get(id: Id[Node]): Option[Node] = this.map.get(id)
+
+  def contains(id: Id[Node]): Boolean = this.map.contains(id)
+
+  def local: Option[Node] = this.localId.flatMap(this.get)
+
+  def operating: Option[Node] = this.operatingId.flatMap(this.get)
+
+  def parents: Seq[Node] = this.parentIds.map(this.get).flatten
+
+  def select(id: Id[Node]): Nodes =
+    if (this.contains(id))
+      this.copy(selectedId = Some(id))
+    else
+      this
+
+  def deselect(): Nodes = this.copy(selectedId = None)
+
+  def isSelecting(id: Id[Node]): Boolean =
+    this.selectedId.map(_ == id).getOrElse(false)
+
+  def selected: Option[Node] = this.selectedId.flatMap(this.get)
+
+  def current: Option[Node] = this.selected.orElse(this.operating)
+}
+
+object Nodes {
+  def apply(info: DatabaseInfo) =
+    new Nodes(
+      map = info.nodes,
+      localId = Some(info.localNodeId),
+      operatingId = Some(info.localNodeId),
+      parentIds = info.parentNodeIds
+    )
+}
