0a917fd0b84634c9b96521740573f554f5e37f96
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-04-17 16:37:18 +0900

    Add PaneStock.pinnedCoto

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/PaneStock.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/PaneStock.scala
@@ -5,7 +5,7 @@ import slinky.web.html._
 
 import cotoami.{Model, Msg}
 import cotoami.backend.{Coto, Link}
-import cotoami.components.ToolButton
+import cotoami.components.{optionalClasses, ToolButton}
 
 object PaneStock {
   val PaneName = "PaneStock"
@@ -18,12 +18,16 @@ object PaneStock {
     section(className := "stock")(
       section(className := "coto-catalog")(
         Option.when(!model.domain.pinnedCotos.isEmpty)(
-          pinned(model.domain.pinnedCotos, dispatch)
+          pinned(model.domain.pinnedCotos, model, dispatch)
         )
       )
     )
 
-  def pinned(pinned: Seq[(Link, Coto)], dispatch: Msg => Unit): ReactElement =
+  def pinned(
+      pinned: Seq[(Link, Coto)],
+      model: Model,
+      dispatch: Msg => Unit
+  ): ReactElement =
     section(className := "pinned header-and-body")(
       header(className := "tools")(
         ToolButton(
@@ -36,6 +40,49 @@ object PaneStock {
           tip = "Document",
           symbol = "view_agenda"
         )
+      ),
+      ol(
+        className := optionalClasses(
+          Seq(
+            ("pinned-cotos", true),
+            ("body", true),
+            ("document-view", true)
+          )
+        )
+      )(
+        pinned.map { case (link, coto) =>
+          li(key := link.id.uuid)(
+            pinnedCoto(coto, model, dispatch)
+          )
+        }: _*
+      )
+    )
+
+  private def pinnedCoto(
+      coto: Coto,
+      model: Model,
+      dispatch: Msg => Unit
+  ): ReactElement =
+    article(
+      className := optionalClasses(
+        Seq(
+          ("pinned-coto", true),
+          ("coto", true),
+          ("has-children", false)
+        )
+      )
+    )(
+      ToolButton(
+        classes = "unpin",
+        tip = "Unpin",
+        tipPlacement = "right",
+        symbol = "push_pin"
+      ),
+      header()(
+        ViewCoto.otherCotonomas(coto, model.domain, dispatch)
+      ),
+      div(className := "body")(
+        ViewCoto.content(coto, s"pinned-${coto.id}", model, dispatch)
       )
     )
 }
