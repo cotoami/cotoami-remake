d0583e0d1fe9176e7c1afc9e1497c1d222788e60
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-05-01 20:22:16 +0900

    refactor(desktop): add cotoami.repositories.Links.mapByTargetCotoId

--- cotoami_desktop/ui/src/main/scala/cotoami/repositories/Links.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/repositories/Links.scala
@@ -1,14 +1,36 @@
 package cotoami.repositories
 
-import scala.collection.immutable.TreeSet
+import scala.collection.immutable.{HashSet, TreeSet}
 import com.softwaremill.quicklens._
 import cotoami.backend._
 
 case class Links(
     map: Map[Id[Link], Link] = Map.empty,
-    mapBySourceCotoId: Map[Id[Coto], TreeSet[Link]] = Map.empty
+    // It breaks SSoT to hold link data duplicated from `map`,
+    // but it's needed to keep them sorted in TreeSet.
+    mapBySourceCotoId: Map[Id[Coto], TreeSet[Link]] = Map.empty,
+    mapByTargetCotoId: Map[Id[Coto], HashSet[Id[Link]]] = Map.empty
 ) {
   def get(id: Id[Link]): Option[Link] = this.map.get(id)
+
+  def add(link: Link): Links = {
+    this
+      .modify(_.map).using(_ + (link.id -> link))
+      .modify(_.mapBySourceCotoId).using(map => {
+        val outgoingLinks =
+          map.get(link.sourceCotoId).map(_ + link)
+            .getOrElse(TreeSet(link))
+        map + (link.sourceCotoId -> outgoingLinks)
+      })
+      .modify(_.mapByTargetCotoId).using(map => {
+        val incomingLinks =
+          map.get(link.targetCotoId).map(_ + link.id)
+            .getOrElse(HashSet(link.id))
+        map + (link.targetCotoId -> incomingLinks)
+      })
+  }
+
+  def addAll(links: Iterable[Link]): Links = links.foldLeft(this)(_ add _)
 
   def linksFrom(id: Id[Coto]): TreeSet[Link] =
     this.mapBySourceCotoId.get(id).getOrElse(TreeSet.empty)
@@ -17,18 +39,6 @@ case class Links(
     this.mapBySourceCotoId.get(id).map(!_.isEmpty).getOrElse(false)
 
   def linksTo(id: Id[Coto]): Seq[Link] =
-    this.map.values.filter(_.targetCotoId == id).toSeq
-
-  def add(link: Link): Links = {
-    this
-      .modify(_.map).using(_ + (link.id -> link))
-      .modify(_.mapBySourceCotoId).using(map => {
-        val links =
-          map.get(link.sourceCotoId).map(_ + link)
-            .getOrElse(TreeSet(link))
-        map + (link.sourceCotoId -> links)
-      })
-  }
-
-  def addAll(links: Iterable[Link]): Links = links.foldLeft(this)(_ add _)
+    this.mapByTargetCotoId.get(id).map(_.map(this.get).flatten.toSeq)
+      .getOrElse(Seq.empty)
 }
