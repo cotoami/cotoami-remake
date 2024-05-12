c8da1b4a77b7da286537b6dd6de1b9c3480c3a0c
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-05-12 18:28:31 +0900

    Add cotoami.subparts.SectionTimeline

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/PaneFlow.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/PaneFlow.scala
@@ -1,12 +1,9 @@
 package cotoami.subparts
 
-import slinky.core.facade.{Fragment, ReactElement}
+import slinky.core.facade.ReactElement
 import slinky.web.html._
 
-import cotoami.{Context, FlowInputMsg, Model, Msg}
-import cotoami.components.{materialSymbol, ScrollArea, ToolButton}
-import cotoami.backend.Coto
-import cotoami.repositories._
+import cotoami.{FlowInputMsg, Model, Msg}
 
 object PaneFlow {
   val PaneName = "PaneFlow"
@@ -39,130 +36,6 @@ object PaneFlow {
           )
         case _ => None
       },
-      Option.when(!model.domain.timeline.isEmpty)(
-        sectionTimeline(model.domain.timeline, model, dispatch)
-      )
-    )
-
-  private def sectionTimeline(
-      cotos: Seq[Coto],
-      model: Model,
-      dispatch: Msg => Unit
-  ): ReactElement =
-    section(className := "timeline header-and-body")(
-      header(className := "tools")(
-        ToolButton(
-          classes = "filter",
-          tip = "Filter",
-          symbol = "filter_list"
-        ),
-        ToolButton(
-          classes = "calendar",
-          tip = "Calendar",
-          symbol = "calendar_month"
-        )
-      ),
-      div(className := "posts body")(
-        ScrollArea(
-          scrollableElementId = None,
-          autoHide = true,
-          bottomThreshold = None,
-          onScrollToBottom = () => dispatch(Msg.FetchMoreTimeline)
-        )(
-          cotos.map(
-            sectionPost(_, model.domain, model.context, dispatch)
-          ) :+ div(
-            className := "more",
-            aria - "busy" := model.domain.cotos.timelineLoading.toString()
-          )(): _*
-        )
-      )
-    )
-
-  private def sectionPost(
-      coto: Coto,
-      domain: Domain,
-      context: Context,
-      dispatch: Msg => Unit
-  ): ReactElement =
-    section(className := "post")(
-      coto.repostOfId.map(_ => repostHeader(coto, domain, dispatch)),
-      ViewCoto.ulParents(domain.parentsOf(coto.id), dispatch),
-      articleCoto(
-        domain.cotos.getOriginal(coto),
-        domain,
-        context,
-        dispatch
-      ),
-      ViewCoto.divLinksTraversal(coto, "top", dispatch)
-    )
-
-  private def articleCoto(
-      coto: Coto,
-      domain: Domain,
-      context: Context,
-      dispatch: Msg => Unit
-  ): ReactElement =
-    article(className := "coto")(
-      header()(
-        ViewCoto.divClassifiedAs(coto, domain, dispatch),
-        Option.when(Some(coto.postedById) != domain.nodes.operatingId) {
-          ViewCoto.addressAuthor(coto, domain.nodes)
-        }
-      ),
-      div(className := "body")(
-        ViewCoto.divContent(coto, domain, dispatch)
-      ),
-      footer()(
-        time(
-          className := "posted-at",
-          title := context.formatDateTime(coto.createdAt)
-        )(
-          context.display(coto.createdAt)
-        )
-      )
-    )
-
-  private def repostHeader(
-      coto: Coto,
-      domain: Domain,
-      dispatch: Msg => Unit
-  ): ReactElement =
-    section(className := "repost-header")(
-      materialSymbol("repeat"),
-      Option.when(domain.cotonomas.selectedId.isEmpty) {
-        repostedIn(coto, domain.cotonomas, dispatch)
-      },
-      Option.when(Some(coto.postedById) != domain.nodes.operatingId) {
-        reposter(coto, domain.nodes)
-      }
-    )
-
-  private def repostedIn(
-      coto: Coto,
-      cotonomas: Cotonomas,
-      dispatch: Msg => Unit
-  ): Option[ReactElement] =
-    coto.postedInId.flatMap(cotonomas.get).map(cotonoma =>
-      a(
-        className := "reposted-in",
-        onClick := ((e) => {
-          e.preventDefault()
-          dispatch(cotoami.SelectCotonoma(cotonoma.id))
-        })
-      )(cotonoma.name)
-    )
-
-  private def reposter(
-      coto: Coto,
-      nodes: Nodes
-  ): ReactElement =
-    address(className := "reposter")(
-      nodes.get(coto.postedById).map(node =>
-        Fragment(
-          nodeImg(node),
-          node.name
-        )
-      )
+      SectionTimeline(model, dispatch)
     )
 }
