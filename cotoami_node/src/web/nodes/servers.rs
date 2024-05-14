6b05b2c984ccc66de1fc89d94921ca2d8ad0f4cb
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-05-14 20:30:49 +0900

    Add NodeState::add_server_node

--- cotoami_node/src/web/nodes/servers.rs
+++ cotoami_node/src/web/nodes/servers.rs
@@ -7,17 +7,16 @@ use axum::{
 };
 use cotoami_db::prelude::*;
 use tokio::task::spawn_blocking;
-use tracing::{debug, info};
+use tracing::debug;
 use validator::Validate;
 
 use crate::{
-    client::HttpClient,
     service::{
         error::IntoServiceResult,
-        models::{AddServerNode, CreateClientNodeSession, Server},
-        RemoteNodeServiceExt, ServiceError,
+        models::{AddServerNode, Server},
+        ServiceError,
     },
-    state::{NodeState, ServerConnection},
+    state::NodeState,
     web::{Accept, Content},
 };
 
@@ -65,82 +64,10 @@ async fn add_server_node(
     TypedHeader(accept): TypedHeader<Accept>,
     Form(form): Form<AddServerNode>,
 ) -> Result<(StatusCode, Content<Server>), ServiceError> {
-    if let Err(errors) = form.validate() {
-        return ("nodes/server", errors).into_result();
-    }
-
-    // Inputs
-    let url_prefix = form.url_prefix.unwrap_or_else(|| unreachable!());
-    let password = form.password.unwrap_or_else(|| unreachable!());
-    let server_as_child = form.as_child.unwrap_or(false);
-
-    // Get the local node
-    let db = state.db().clone();
-    let local_node = spawn_blocking(move || db.new_session()?.local_node()).await??;
-
-    // Attempt to log into the server node
-    let mut http_client = HttpClient::new(url_prefix)?;
-    let client_session = http_client
-        .create_client_node_session(CreateClientNodeSession {
-            password: password.clone(),
-            new_password: None, // TODO: change the password on the first login
-            client: local_node,
-            as_parent: Some(server_as_child),
-        })
-        .await?;
-    info!("Successfully logged in to {}", http_client.url_prefix());
-    let server_id = client_session.server.uuid;
-
-    // Register the server node
-    let (server, server_node, server_db_role) = spawn_blocking({
-        let state = state.clone();
-        let operator = operator.clone();
-        let url_prefix = http_client.url_prefix().to_string();
-        move || {
-            let mut ds = state.db().new_session()?;
-
-            // Import the server node data, which is required for registering a [ServerNode]
-            if let Some((_, changelog)) = ds.import_node(&client_session.server)? {
-                state.pubsub().publish_change(changelog);
-            }
-
-            // Database role
-            let server_db_role = if server_as_child {
-                NewDatabaseRole::Child {
-                    as_owner: false,
-                    can_edit_links: false,
-                }
-            } else {
-                NewDatabaseRole::Parent
-            };
-
-            // Register a [ServerNode] and save the password into it
-            let owner_password = state.config().try_get_owner_password()?;
-            let (_, server_db_role) =
-                ds.register_server_node(&server_id, &url_prefix, server_db_role, &operator)?;
-            let server =
-                ds.save_server_password(&server_id, &password, owner_password, &operator)?;
-
-            // Get the imported node data
-            let node = ds.node(&server_id)?.unwrap_or_else(|| unreachable!());
-            Ok::<_, ServiceError>((server, node, server_db_role))
-        }
-    })
-    .await??;
-    info!("ServerNode [{}] registered.", server_node.name);
-
-    // Create a ServerConnection
-    let server_conn = ServerConnection::new(&server, http_client.clone(), &state).await?;
-    state.put_server_conn(&server_id, server_conn.clone());
-
-    // Return a Server as a response
-    let server = Server::new(
-        server_node,
-        server,
-        server_conn.not_connected(),
-        Some(server_db_role),
-    );
-    Ok((StatusCode::CREATED, Content(server, accept)))
+    state
+        .add_server_node(form, &operator)
+        .await
+        .map(|server| (StatusCode::CREATED, Content(server, accept)))
 }
 
 /////////////////////////////////////////////////////////////////////////////
