f8bd6846bb311e99b401120170fc6f58cfef0818
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-05-16 21:47:20 +0900

    Add cotoami.backend.ChangelogEntryJson

--- cotoami_desktop/ui/src/main/scala/cotoami/backend/Change.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/backend/Change.scala
@@ -3,6 +3,16 @@ package cotoami.backend
 import scala.scalajs.js
 
 @js.native
+trait ChangelogEntryJson extends js.Object {
+  val serial_number: Double = js.native
+  val origin_node_id: String = js.native
+  val origin_serial_number: Double = js.native
+  val type_number: Int = js.native
+  val change: ChangeJson = js.native
+  val inserted_at: String = js.native
+}
+
+@js.native
 trait ChangeJson extends js.Object {
   val None: js.UndefOr[scala.Null] = js.native
   val CreateNode: js.UndefOr[CreateNodeJson] = js.native
