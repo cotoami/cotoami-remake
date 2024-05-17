1bf354a407cd5757aac0e18b41a55447e0471f4e
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-05-17 20:11:14 +0900

    Add cotoami.backend.ChangeJson

--- cotoami_desktop/ui/src/main/scala/cotoami/backend/Change.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/backend/Change.scala
@@ -19,6 +19,16 @@ trait ChangeJson extends js.Object {
   val UpsertNode: js.UndefOr[NodeJson] = js.native
   val RenameNode: js.UndefOr[RenameNodeJson] = js.native
   val SetRootCotonoma: js.UndefOr[SetRootCotonomaJson] = js.native
+  val CreateCoto: js.UndefOr[CotoJson] = js.native
+  val EditCoto: js.UndefOr[EditCotoJson] = js.native
+  val DeleteCoto: js.UndefOr[String] = js.native
+  val CreateCotonoma: js.UndefOr[js.Tuple2[CotonomaJson, CotoJson]] = js.native
+  val RenameCotonoma: js.UndefOr[RenameCotonomaJson] = js.native
+  val DeleteCotonoma: js.UndefOr[String] = js.native
+  val CreateLink: js.UndefOr[LinkJson] = js.native
+  val EditLink: js.UndefOr[EditLinkJson] = js.native
+  val DeleteLink: js.UndefOr[String] = js.native
+  val ChangeOwnerNode: js.UndefOr[ChangeOwnerNodeJson] = js.native
 }
 
 @js.native
@@ -38,4 +48,34 @@ trait RenameNodeJson extends js.Object {
 trait SetRootCotonomaJson extends js.Object {
   val node_id: String = js.native
   val cotonoma_id: String = js.native
+}
+
+@js.native
+trait EditCotoJson extends js.Object {
+  val coto_id: String = js.native
+  val content: String = js.native
+  val summary: String = js.native
+  val updated_at: String = js.native
+}
+
+@js.native
+trait RenameCotonomaJson extends js.Object {
+  val cotonoma_id: String = js.native
+  val name: String = js.native
+  val updated_at: String = js.native
+}
+
+@js.native
+trait EditLinkJson extends js.Object {
+  val link_id: String = js.native
+  val linking_phrase: String = js.native
+  val details: String = js.native
+  val updated_at: String = js.native
+}
+
+@js.native
+trait ChangeOwnerNodeJson extends js.Object {
+  val from: String = js.native
+  val to: String = js.native
+  val last_change_number: Double = js.native
 }
