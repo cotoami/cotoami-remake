21edf382d6fc3e335e430e2afebbf821ca3dd1d2
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-06-07 19:53:25 +0900

    feat(desktop): update Modal.Stack to use a type to select a modal

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/Modal.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/Modal.scala
@@ -1,7 +1,7 @@
 package cotoami.subparts
 
 import scala.util.chaining._
-import scala.reflect.ClassTag
+import scala.reflect.{classTag, ClassTag}
 import slinky.core.facade.ReactElement
 import com.softwaremill.quicklens._
 
@@ -18,17 +18,31 @@ object Modal {
   }
 
   case class Stack(modals: Seq[Model] = Seq.empty) {
-    def open(modal: Model): Stack = this.modify(_.modals).using(modal +: _)
+    def open[M <: Model: ClassTag](modal: M): Stack =
+      this.close[M].modify(_.modals).using(modal +: _)
 
     def top: Option[Model] = this.modals.headOption
 
-    def updateTop(modal: Model): Stack =
-      this.modify(_.modals).using(_.updated(0, modal))
+    def get[M <: Model: ClassTag]: Option[M] =
+      this.modals.find(classTag[M].runtimeClass.isInstance(_))
+        .map(_.asInstanceOf[M])
+
+    def update[M <: Model: ClassTag](newState: M): Stack =
+      this.modify(_.modals).using(
+        _.map(modal =>
+          if (classTag[M].runtimeClass.isInstance(modal))
+            newState
+          else
+            modal
+        )
+      )
 
     def closeTop: Stack = this.modify(_.modals).using(_.drop(1))
 
-    def close[M >: Model: ClassTag]: Stack =
-      this.modify(_.modals).using(_.filterNot(_.isInstanceOf[M]))
+    def close[M <: Model: ClassTag]: Stack =
+      this.modify(_.modals).using(
+        _.filterNot(classTag[M].runtimeClass.isInstance(_))
+      )
   }
 
   sealed trait Msg {
@@ -39,24 +53,26 @@ object Modal {
   case class AddNodeMsg(msg: ModalAddNode.Msg) extends Msg
 
   def update(msg: Msg, stack: Stack): (Stack, Seq[Cmd[cotoami.Msg]]) =
-    (msg, stack.top) match {
-      case (OpenModal(modal), _) =>
-        (stack.open(modal), Seq.empty)
+    (msg match {
+      case OpenModal(modal) =>
+        Some((stack.open(modal), Seq.empty))
 
-      case (WelcomeMsg(modalMsg), Some(WelcomeModel(modalModel))) =>
-        ModalWelcome.update(modalMsg, modalModel)
-          .pipe { case (model, cmds) =>
-            (stack.updateTop(WelcomeModel(model)), cmds)
-          }
+      case WelcomeMsg(modalMsg) =>
+        stack.get[WelcomeModel].map { case WelcomeModel(modalModel) =>
+          ModalWelcome.update(modalMsg, modalModel)
+            .pipe { case (model, cmds) =>
+              (stack.update(WelcomeModel(model)), cmds)
+            }
+        }
 
-      case (AddNodeMsg(modalMsg), Some(AddNodeModel(modalModel))) =>
-        ModalAddNode.update(modalMsg, modalModel)
-          .pipe { case (model, cmds) =>
-            (stack.updateTop(AddNodeModel(model)), cmds)
-          }
-
-      case (_, _) => (stack, Seq.empty)
-    }
+      case AddNodeMsg(modalMsg) =>
+        stack.get[AddNodeModel].map { case AddNodeModel(modalModel) =>
+          ModalAddNode.update(modalMsg, modalModel)
+            .pipe { case (model, cmds) =>
+              (stack.update(AddNodeModel(model)), cmds)
+            }
+        }
+    }).getOrElse((stack, Seq.empty))
 
   def apply(
       model: cotoami.Model,
