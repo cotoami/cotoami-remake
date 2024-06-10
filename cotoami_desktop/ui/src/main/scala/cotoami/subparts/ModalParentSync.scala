ff08a0c633aaa6630ef776affe72636d608ae662
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-06-10 15:17:16 +0900

    Add ModalParentSync.sectionSyncing

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/ModalParentSync.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/ModalParentSync.scala
@@ -1,8 +1,10 @@
 package cotoami.subparts
 
 import slinky.core.facade.ReactElement
+import slinky.web.html
 import slinky.web.html._
 
+import cotoami.backend.Id
 import cotoami.repositories.Domain
 import cotoami.models.ParentSync
 
@@ -23,8 +25,41 @@ object ModalParentSync {
           h1()("Syncing with remote nodes")
         ),
         div(className := "body")(
-          //
+          Option.when(!parentSync.syncing.isEmpty) {
+            sectionSyncing(parentSync, domain, dispatch)
+          }
         )
+      )
+    )
+
+  private def sectionSyncing(
+      parentSync: ParentSync,
+      domain: Domain,
+      dispatch: cotoami.Msg => Unit
+  ): ReactElement =
+    section(className := "syncing")(
+      h2()("Syncing"),
+      ul()(
+        parentSync.syncing.map(progress => {
+          li()(
+            domain.nodes.get(Id(progress.node_id)).map(node =>
+              section(className := "parent-node")(
+                nodeImg(node),
+                span(className := "name")(node.name)
+              )
+            ).getOrElse(
+              section(className := "parent-node not-found")(
+                s"Node not found: ${progress.node_id}"
+              )
+            ),
+            div(className := "progress")(
+              html.progress(
+                value := progress.progress.toString(),
+                max := progress.total.toString()
+              )
+            )
+          )
+        })
       )
     )
 }
