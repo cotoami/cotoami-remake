ba7f41ad036b26626877a8ec45d8961010803746
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-06-10 16:37:22 +0900

    Add ModalParentSync.sectionSynced

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/ModalParentSync.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/ModalParentSync.scala
@@ -4,7 +4,7 @@ import slinky.core.facade.ReactElement
 import slinky.web.html
 import slinky.web.html._
 
-import cotoami.backend.Id
+import cotoami.backend.{Id, Node, Nullable}
 import cotoami.repositories.Domain
 import cotoami.models.ParentSync
 
@@ -27,6 +27,9 @@ object ModalParentSync {
         div(className := "body")(
           Option.when(!parentSync.syncing.isEmpty) {
             sectionSyncing(parentSync, domain, dispatch)
+          },
+          Option.when(!parentSync.synced.isEmpty) {
+            sectionSynced(parentSync, domain, dispatch)
           }
         )
       )
@@ -41,25 +44,59 @@ object ModalParentSync {
       h2()("Syncing"),
       ul()(
         parentSync.syncing.map(progress => {
+          val current = progress.progress.toString()
+          val total = progress.total.toString()
           li()(
-            domain.nodes.get(Id(progress.node_id)).map(node =>
-              section(className := "parent-node")(
-                nodeImg(node),
-                span(className := "name")(node.name)
-              )
-            ).getOrElse(
-              section(className := "parent-node not-found")(
-                s"Node not found: ${progress.node_id}"
-              )
+            section(className := "parent-node")(
+              spanNode(Id(progress.node_id), domain),
+              span(className := "progress")(s"${current}/${total}")
             ),
-            div(className := "progress")(
-              html.progress(
-                value := progress.progress.toString(),
-                max := progress.total.toString()
+            div(className := "progress-bar")(
+              html.progress(value := current, max := total)
+            )
+          )
+        })
+      )
+    )
+
+  private def sectionSynced(
+      parentSync: ParentSync,
+      domain: Domain,
+      dispatch: cotoami.Msg => Unit
+  ): ReactElement =
+    section(className := "synced")(
+      h2()("Synced"),
+      ul()(
+        parentSync.synced.map(done => {
+          li()(
+            section(className := "parent-node")(
+              spanNode(Id(done.node_id), domain),
+              Nullable.toOption(done.range).map(range =>
+                span(className := "result")(
+                  span(className := "range")(s"${range._1} to ${range._2}"),
+                  span(className := "changes")(
+                    s"(${range._2 - range._1} changes)"
+                  )
+                )
+              ),
+              Nullable.toOption(done.error).map(error =>
+                span(className := "error")(error)
               )
             )
           )
         })
+      )
+    )
+
+  private def spanNode(id: Id[Node], domain: Domain): ReactElement =
+    domain.nodes.get(id).map(node =>
+      span(className := "node")(
+        nodeImg(node),
+        span(className := "name")(node.name)
+      )
+    ).getOrElse(
+      span(className := "node not-found")(
+        s"Node not found: ${id}"
       )
     )
 }
