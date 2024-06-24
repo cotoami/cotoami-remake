f74e5dbfcdfbca677c0b94a6ee30fd9ca95a5159
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-06-24 21:11:06 +0900

    Add db::transactions::changes

--- cotoami_db/src/db/transactions.rs
+++ cotoami_db/src/db/transactions.rs
@@ -16,6 +16,7 @@ use crate::{
     models::prelude::*,
 };
 
+pub mod changes;
 pub mod nodes;
 
 pub struct DatabaseSession<'a> {
@@ -42,31 +43,6 @@ impl<'a> DatabaseSession<'a> {
             new_ro_conn,
             lock_rw_conn,
         }
-    }
-
-    /////////////////////////////////////////////////////////////////////////////
-    // changelog
-    /////////////////////////////////////////////////////////////////////////////
-
-    pub fn import_change(
-        &self,
-        log: &ChangelogEntry,
-        parent_node_id: &Id<Node>,
-    ) -> Result<Option<ChangelogEntry>> {
-        let mut parent_node = self.globals.try_write_parent_node(parent_node_id)?;
-        self.write_transaction(changelog_ops::import_change(log, &mut parent_node))
-    }
-
-    pub fn chunk_of_changes(
-        &mut self,
-        from: i64,
-        limit: i64,
-    ) -> Result<(Vec<ChangelogEntry>, i64)> {
-        self.read_transaction(changelog_ops::chunk(from, limit))
-    }
-
-    pub fn last_change_number(&mut self) -> Result<Option<i64>> {
-        self.read_transaction(changelog_ops::last_serial_number())
     }
 
     /////////////////////////////////////////////////////////////////////////////
