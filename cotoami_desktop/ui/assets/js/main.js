126a1ae551debe620dfd79f303af4c536070acfd
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-07-25 11:26:31 +0900

    fix(ui): prevent the WebView from loading a drag-and-dropped file

--- cotoami_desktop/ui/assets/js/main.js
+++ cotoami_desktop/ui/assets/js/main.js
@@ -1,2 +1,11 @@
 // Execute the main method of the Scala.js application.
 import 'scalajs:main.js'
+
+// Prevent the WebView from loading a drag-and-dropped file
+// https://stackoverflow.com/a/6756680
+window.addEventListener("dragover", e => {
+  e.preventDefault();
+}, false);
+window.addEventListener("drop", e => {
+  e.preventDefault();
+}, false);
