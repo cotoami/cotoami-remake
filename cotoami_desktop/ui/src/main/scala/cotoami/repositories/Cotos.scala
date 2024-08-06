dc7fb02cd118b917350a82e4dddd0fe5ff6d5022
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-08-06 13:59:27 +0900

    Add Cotos.focusedId

--- cotoami_desktop/ui/src/main/scala/cotoami/repositories/Cotos.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/repositories/Cotos.scala
@@ -4,12 +4,15 @@ import com.softwaremill.quicklens._
 import cotoami.backend._
 
 case class Cotos(
-    map: Map[Id[Coto], Coto] = Map.empty
+    map: Map[Id[Coto], Coto] = Map.empty,
+    focusedId: Option[Id[Coto]] = None
 ) {
   def get(id: Id[Coto]): Option[Coto] = this.map.get(id)
 
   def getOriginal(coto: Coto): Coto =
     coto.repostOfId.flatMap(this.get).getOrElse(coto)
+
+  def contains(id: Id[Coto]): Boolean = this.map.contains(id)
 
   def put(coto: Coto): Cotos =
     this.modify(_.map).using(_ + (coto.id -> coto))
@@ -25,4 +28,17 @@ case class Cotos(
     this
       .putAll(graph.cotos)
       .putAll(graph.cotosRelatedData.originals)
+
+  def isFocusing(id: Id[Coto]): Boolean =
+    this.focusedId.map(_ == id).getOrElse(false)
+
+  def focused: Option[Coto] = this.focusedId.flatMap(this.get)
+
+  def focus(id: Option[Id[Coto]]): Cotos =
+    if (id.map(this.contains(_)).getOrElse(true))
+      this.copy(focusedId = id)
+    else
+      this
+
+  def unfocus: Cotos = this.copy(focusedId = None)
 }
