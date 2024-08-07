aed36b92094bc30c023acba9224fbd70d42b8a54
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-08-07 13:52:35 +0900

    Add SectionCotoDetails.olSubCotos

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/SectionCotoDetails.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/SectionCotoDetails.scala
@@ -5,7 +5,7 @@ import slinky.web.html._
 
 import cotoami.{Msg => AppMsg}
 import cotoami.Context
-import cotoami.backend.Coto
+import cotoami.backend.{Coto, Link}
 import cotoami.components.{toolButton, ScrollArea}
 
 object SectionCotoDetails {
@@ -25,17 +25,20 @@ object SectionCotoDetails {
       ),
       div(className := "body")(
         ScrollArea()(
-          articleCotoMain(coto)
+          articleMainCoto(coto),
+          div(className := "sub-cotos")(
+            olSubCotos(coto)
+          )
         )
       )
     )
 
-  private def articleCotoMain(coto: Coto)(implicit
+  private def articleMainCoto(coto: Coto)(implicit
       context: Context,
       dispatch: AppMsg => Unit
   ): ReactElement = {
     val domain = context.domain
-    article(className := "coto main")(
+    article(className := "main-coto coto")(
       header()(
         ViewCoto.divClassifiedAs(coto),
         ViewCoto.addressAuthor(coto, domain.nodes)
@@ -53,4 +56,44 @@ object SectionCotoDetails {
       )
     )
   }
+
+  private def olSubCotos(coto: Coto)(implicit
+      context: Context,
+      dispatch: AppMsg => Unit
+  ): ReactElement = {
+    val subCotos = context.domain.childrenOf(coto.id)
+    ol(className := "sub-cotos")(
+      subCotos.map { case (link, subCoto) =>
+        liSubCoto(link, subCoto)
+      }: _*
+    )
+  }
+
+  private def liSubCoto(
+      link: Link,
+      coto: Coto
+  )(implicit context: Context, dispatch: AppMsg => Unit): ReactElement =
+    li(key := link.id.uuid, className := "sub")(
+      ViewCoto.ulParents(
+        context.domain.parentsOf(coto.id).filter(_._2.id != link.id)
+      ),
+      article(
+        className := "sub-coto coto",
+        onClick := (_ => dispatch(AppMsg.FocusCoto(coto.id)))
+      )(
+        header()(
+          toolButton(
+            symbol = "subdirectory_arrow_right",
+            tip = "Unlink",
+            tipPlacement = "right",
+            classes = "unlink"
+          ),
+          ViewCoto.divClassifiedAs(coto)
+        ),
+        div(className := "body")(
+          ViewCoto.divContent(coto)
+        )
+      ),
+      ViewCoto.divLinksTraversal(coto, "left")
+    )
 }
