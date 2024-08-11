52ad50a7ceaaa4e11f5a59912982243a29ada7ad
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-08-12 00:50:10 +0900

    fix(ui): the map won't fit to the container when first loaded

--- cotoami_desktop/ui/src/main/scala/cotoami/components/MapLibre.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/components/MapLibre.scala
@@ -18,13 +18,17 @@ import cotoami.maplibre._
   val component = FunctionalComponent[Props] { props =>
     useEffect(
       () => {
-        val map = new Map(new MapOptions {
-          override val container = props.id
-          override val zoom = 6
-          override val center = js.Tuple2.fromScalaTuple2(props.defaultPosition)
-          override val style =
-            "https://tile.openstreetmap.jp/styles/osm-bright-en/style.json"
-        })
+        // Delay rendering the map to ensure it to fit to the container section.
+        js.timers.setTimeout(10) {
+          val map = new Map(new MapOptions {
+            override val container = props.id
+            override val zoom = 6
+            override val center =
+              js.Tuple2.fromScalaTuple2(props.defaultPosition)
+            override val style =
+              "https://tile.openstreetmap.jp/styles/osm-bright-en/style.json"
+          })
+        }
       },
       Seq.empty
     )
