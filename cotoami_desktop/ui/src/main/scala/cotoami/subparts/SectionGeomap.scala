f239ba1d7856ce0aecd0e77426e72cf9e807cb3f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-08-24 11:50:11 +0900

    refactor(ui): minor refactoring in SectionGeomap

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/SectionGeomap.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/SectionGeomap.scala
@@ -4,7 +4,6 @@ import slinky.core.facade.ReactElement
 
 import fui.Cmd
 import cotoami.{Msg => AppMsg}
-import cotoami.libs.geomap.maplibre
 import cotoami.components.MapLibre
 import cotoami.models.{Geolocation, Geomap}
 
@@ -16,20 +15,18 @@ object SectionGeomap {
 
   object Msg {
     case object MapInit extends Msg
-    case class MapClicked(lngLat: maplibre.LngLat) extends Msg
+    case class MapClicked(location: Geolocation) extends Msg
     case class MapZoomChanged(zoom: Double) extends Msg
-    case class MapCenterMoved(center: (Double, Double)) extends Msg
+    case class MapCenterMoved(center: Geolocation) extends Msg
   }
 
   def update(msg: Msg, geomap: Geomap): (Geomap, Seq[Cmd[AppMsg]]) =
     msg match {
       case Msg.MapInit => (geomap, Seq.empty)
 
-      case Msg.MapClicked(lngLat) =>
+      case Msg.MapClicked(location) =>
         (
-          geomap.copy(focusedLocation =
-            Some(Geolocation.fromLngLat(lngLat.toArray()))
-          ),
+          geomap.copy(focusedLocation = Some(location)),
           Seq.empty
         )
 
@@ -37,7 +34,7 @@ object SectionGeomap {
         (geomap.copy(zoom = zoom), Seq.empty)
 
       case Msg.MapCenterMoved(center) =>
-        (geomap.copy(center = Geolocation.fromLngLat(center)), Seq.empty)
+        (geomap.copy(center = center), Seq.empty)
     }
 
   def apply(geomap: Geomap)(implicit dispatch: AppMsg => Unit): ReactElement =
@@ -48,9 +45,14 @@ object SectionGeomap {
       syncCenterZoom = geomap.syncCenterZoom,
       focusedLocation = geomap.focusedLocation.map(toLngLat),
       onInit = Some(() => dispatch(Msg.MapInit.toApp)),
-      onClick = Some(e => dispatch(Msg.MapClicked(e.lngLat).toApp)),
+      onClick = Some(e => {
+        val location = Geolocation.fromLngLat(e.lngLat.toArray())
+        dispatch(Msg.MapClicked(location).toApp)
+      }),
       onZoomChanged = Some(zoom => dispatch(Msg.MapZoomChanged(zoom).toApp)),
-      onCenterMoved = Some(center => dispatch(Msg.MapCenterMoved(center).toApp))
+      onCenterMoved = Some(center =>
+        dispatch(Msg.MapCenterMoved(Geolocation.fromLngLat(center)).toApp)
+      )
     )
 
   private def toLngLat(location: Geolocation): (Double, Double) =
