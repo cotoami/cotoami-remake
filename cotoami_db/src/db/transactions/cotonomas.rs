538190fd690f8521db920d528e98d69131c370e3
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-09-27 15:55:44 +0900

    Add DatabaseSession::all_node_roots

--- cotoami_db/src/db/transactions/cotonomas.rs
+++ cotoami_db/src/db/transactions/cotonomas.rs
@@ -1,11 +1,12 @@
 use std::collections::HashSet;
 
 use anyhow::Result;
+use diesel::sqlite::SqliteConnection;
 
 use crate::{
     db::{
         op::*,
-        ops::{changelog_ops, coto_ops, cotonoma_ops, Paginated},
+        ops::{changelog_ops, coto_ops, cotonoma_ops, node_ops, Paginated},
         DatabaseSession,
     },
     models::prelude::*,
@@ -18,6 +19,13 @@ impl<'a> DatabaseSession<'a> {
         } else {
             Ok(None)
         }
+    }
+
+    pub fn all_node_roots(&mut self) -> Result<Vec<(Cotonoma, Coto)>> {
+        self.read_transaction(|ctx: &mut Context<'_, SqliteConnection>| {
+            let cotonoma_ids = node_ops::root_cotonoma_ids().run(ctx)?;
+            cotonoma_ops::get_pairs_by_ids(cotonoma_ids).run(ctx)
+        })
     }
 
     pub fn cotonoma(&mut self, id: &Id<Cotonoma>) -> Result<Option<(Cotonoma, Coto)>> {
