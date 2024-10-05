7331b58f5882f78f4d5560f2127c7e0fbe439275
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-10-05 22:14:28 +0900

    refactor(node): move Command to service::command

--- cotoami_node/src/service.rs
+++ cotoami_node/src/service.rs
@@ -24,12 +24,14 @@ use serde::de::DeserializeOwned;
 use thiserror::Error;
 use uuid::Uuid;
 
+pub mod command;
 pub mod error;
 pub mod models;
 pub mod pubsub;
 pub mod service_ext;
 
 pub(crate) use self::{
+    command::Command,
     error::*,
     models::*,
     pubsub::*,
@@ -134,112 +136,6 @@ impl Request {
     pub fn operate_as_owner(&mut self) { self.as_owner = true }
 
     pub fn command(self) -> Command { self.command }
-}
-
-#[derive(derive_more::Debug, Clone, serde::Serialize, serde::Deserialize)]
-pub enum Command {
-    /// Request the local node as a [Node].
-    LocalNode,
-
-    /// Request an [InitialDataset].
-    InitialDataset,
-
-    /// Request a [ChunkOfChanges] from a change number `from`.
-    ChunkOfChanges { from: i64 },
-
-    /// Request to change the icon of the local node and and return a [Node] if succeeded.
-    SetLocalNodeIcon {
-        #[debug(skip)]
-        icon: Bytes,
-    },
-
-    /// Request a [NodeDetails] of the given ID.
-    NodeDetails { id: Id<Node> },
-
-    /// Request a new [ClientNodeSession] on the local node.
-    CreateClientNodeSession(CreateClientNodeSession),
-
-    /// Request to log into the server node and return a [ClientNodeSession] if succeeded.
-    TryLogIntoServer(LogIntoServer),
-
-    /// Request to add a new [Server].
-    AddServer(LogIntoServer),
-
-    /// Request to update a server node and return an updated [ServerNode].
-    UpdateServer { id: Id<Node>, values: UpdateServer },
-
-    /// Request a [Page<ClientNode>] that contains recently registered clients.
-    RecentClients { pagination: Pagination },
-
-    /// Request to add a new client node and return [ClientAdded] if succeeded.
-    AddClient(AddClient),
-
-    /// Request a [Page<Cotonoma>] that contains recently updated cotonomas.
-    RecentCotonomas {
-        node: Option<Id<Node>>,
-        pagination: Pagination,
-    },
-
-    /// Request a tuple of [Cotonoma] and [Coto] `(Cotonoma, Coto)` of the given cotonoma ID.
-    Cotonoma { id: Id<Cotonoma> },
-
-    /// Request a [CotonomaDetails] of the given ID.
-    CotonomaDetails { id: Id<Cotonoma> },
-
-    /// Request a [Cotonoma] of the given name in the given node.
-    CotonomaByName { name: String, node: Id<Node> },
-
-    /// Request a [Page<Cotonoma>] that contains sub cotonomas of the given cotonoma.
-    SubCotonomas {
-        id: Id<Cotonoma>,
-        pagination: Pagination,
-    },
-
-    /// Request [CotosPage] that contains recently posted cotos.
-    RecentCotos {
-        node: Option<Id<Node>>,
-        cotonoma: Option<Id<Cotonoma>>,
-        pagination: Pagination,
-    },
-
-    /// Request [GeolocatedCotos] in the given node or cotonoma.
-    GeolocatedCotos {
-        node: Option<Id<Node>>,
-        cotonoma: Option<Id<Cotonoma>>,
-    },
-
-    /// Request [GeolocatedCotos] found in the given geo bounds.
-    CotosInGeoBounds {
-        southwest: Geolocation,
-        northeast: Geolocation,
-    },
-
-    /// Request [CotosPage] that match the given query and cotonoma.
-    SearchCotos {
-        query: String,
-        node: Option<Id<Node>>,
-        cotonoma: Option<Id<Cotonoma>>,
-        pagination: Pagination,
-    },
-
-    /// Request a [CotoGraph] by traversing from the given coto.
-    GraphFromCoto { coto: Id<Coto> },
-
-    /// Request a [CotoGraph] by traversing from the given cotonoma.
-    GraphFromCotonoma { cotonoma: Id<Cotonoma> },
-
-    /// Request to create a new [Coto] in the given cotonoma (`post_to`).
-    PostCoto {
-        input: CotoInput<'static>,
-        post_to: Id<Cotonoma>,
-    },
-
-    /// Request to create a new [Cotonoma] in the given cotonoma (`post_to`).
-    /// The return type is a tuple of [Cotonoma] and [Coto] `(Cotonoma, Coto)`.
-    PostCotonoma {
-        input: CotonomaInput<'static>,
-        post_to: Id<Cotonoma>,
-    },
 }
 
 impl Command {
