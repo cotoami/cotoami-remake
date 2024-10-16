72f7f963f2f5815a5121017754e7a7ab5d1dd548
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-10-16 21:51:57 +0900

    feat(ui): control the visibility of buttons in ToolbarCoto

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/ToolbarCoto.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/ToolbarCoto.scala
@@ -3,25 +3,30 @@ package cotoami.subparts
 import slinky.core.facade.ReactElement
 import slinky.web.html._
 
+import cotoami.Context
 import cotoami.models.Coto
 import cotoami.components.toolButton
 
 object ToolbarCoto {
 
-  def apply(coto: Coto): ReactElement =
+  def apply(coto: Coto)(implicit context: Context): ReactElement =
     section(className := "coto-toolbar")(
-      toolButton(
-        symbol = "push_pin",
-        tip = "Pin",
-        tipPlacement = "left",
-        classes = "pin-coto"
-      ),
-      toolButton(
-        symbol = "edit",
-        tip = "Edit",
-        tipPlacement = "left",
-        classes = "edit-coto"
-      ),
+      Option.when(context.domain.canEditLinks) {
+        toolButton(
+          symbol = "push_pin",
+          tip = "Pin",
+          tipPlacement = "left",
+          classes = "pin-coto"
+        )
+      },
+      Option.when(context.domain.nodes.canEdit(coto)) {
+        toolButton(
+          symbol = "edit",
+          tip = "Edit",
+          tipPlacement = "left",
+          classes = "edit-coto"
+        )
+      },
       toolButton(
         symbol = "repeat",
         tip = "Repost",
