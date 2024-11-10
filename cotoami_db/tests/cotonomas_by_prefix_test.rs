d471b5339d4a946a72a1c36bba459c2fb5b8e66c
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-11-10 19:04:59 +0900

    test(db): add test cases in cotonomas_by_prefix_test

--- cotoami_db/tests/cotonomas_by_prefix_test.rs
+++ cotoami_db/tests/cotonomas_by_prefix_test.rs
@@ -17,7 +17,25 @@ fn cotonomas_by_prefix() -> Result<()> {
     let _ = ds.post_cotonoma(&CotonomaInput::new("foo"), &root, &opr)?;
     let _ = ds.post_cotonoma(&CotonomaInput::new("abcabc"), &root, &opr)?;
 
-    // When
+    // When: limit 1 (only exact matches)
+    let cotonomas = ds.cotonomas_by_prefix("abc", None, 1)?;
+    assert_that!(
+        cotonomas,
+        elements_are![pat!(Cotonoma { name: eq("abc") }),]
+    );
+
+    // When: limit 2 (both of exact and prefix matches)
+    let cotonomas = ds.cotonomas_by_prefix("abc", None, 2)?;
+    assert_that!(
+        cotonomas,
+        elements_are![
+            // exact matches should come first
+            pat!(Cotonoma { name: eq("abc") }),
+            pat!(Cotonoma { name: eq("abcabc") }),
+        ]
+    );
+
+    // When: limit 5 (all)
     let cotonomas = ds.cotonomas_by_prefix("abc", None, 5)?;
     assert_that!(
         cotonomas,
