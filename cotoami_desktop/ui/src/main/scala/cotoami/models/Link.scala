c22f9a6b5c165cc645f8657450a5bf903b0d1334
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-12-02 18:33:54 +0900

    refactor(ui): delete redundant 'this' from cotoami.models.Link

--- cotoami_desktop/ui/src/main/scala/cotoami/models/Link.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/models/Link.scala
@@ -16,8 +16,8 @@ case class Link(
     createdAtUtcIso: String,
     updatedAtUtcIso: String
 ) extends Entity[Link] {
-  lazy val createdAt: Instant = parseUtcIso(this.createdAtUtcIso)
-  lazy val updatedAt: Instant = parseUtcIso(this.updatedAtUtcIso)
+  lazy val createdAt: Instant = parseUtcIso(createdAtUtcIso)
+  lazy val updatedAt: Instant = parseUtcIso(updatedAtUtcIso)
 }
 
 object Link {
