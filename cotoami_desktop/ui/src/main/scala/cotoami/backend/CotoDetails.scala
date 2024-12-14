92205ed8c92c86d374de29383312d8439dc1e888
Daisuke Morita <daisuke.marubinotto@gmail.com>
2024-12-14 18:50:13 +0900

    feat(ui): add CotoDetailsJson.fetch

--- cotoami_desktop/ui/src/main/scala/cotoami/backend/CotoDetails.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/backend/CotoDetails.scala
@@ -1,10 +1,20 @@
 package cotoami.backend
 
 import scala.scalajs.js
+import fui.Cmd
+
+import cotoami.models.{Coto, Id}
 
 @js.native
 trait CotoDetailsJson extends js.Object {
   val coto: CotoJson = js.native
   val related_data: CotosRelatedDataJson = js.native
   val outgoing_links: js.Array[LinkJson] = js.native
+}
+
+object CotoDetailsJson {
+  def fetch(
+      id: Id[Coto]
+  ): Cmd.One[Either[ErrorJson, CotoDetailsJson]] =
+    Commands.send(Commands.CotoDetails(id))
 }
