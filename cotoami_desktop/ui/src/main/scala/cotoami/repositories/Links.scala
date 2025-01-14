4bd0f4d5c264bfd741fee2b24997a2328112cd39
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-01-14 22:52:50 +0900

    feat(ui): add Links.delete

--- cotoami_desktop/ui/src/main/scala/cotoami/repositories/Links.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/repositories/Links.scala
@@ -33,6 +33,19 @@ case class Links(
 
   def putAll(links: Iterable[Link]): Links = links.foldLeft(this)(_ put _)
 
+  def delete(id: Id[Link]): Links = {
+    this
+      .modify(_.map).using(_ - id)
+      .modify(_.mapBySourceCotoId).using(
+        _.map { case (cotoId, links) => (cotoId, links.filterNot(_.id == id)) }
+          .filterNot(_._2.isEmpty)
+      )
+      .modify(_.mapByTargetCotoId).using(
+        _.map { case (cotoId, linkIds) => (cotoId, linkIds - id) }
+          .filterNot(_._2.isEmpty)
+      )
+  }
+
   def linked(from: Id[Coto], to: Id[Coto]): Boolean =
     mapByTargetCotoId.get(to).map(
       _.exists(get(_).map(_.sourceCotoId == from).getOrElse(false))
@@ -49,11 +62,7 @@ case class Links(
       .getOrElse(Seq.empty)
 
   def onCotoDelete(id: Id[Coto]): Links = {
-    val linkIdsToDelete = (from(id).toSeq ++ to(id)).map(_.id)
-    copy(
-      map = map -- linkIdsToDelete,
-      mapBySourceCotoId = mapBySourceCotoId - id,
-      mapByTargetCotoId = mapByTargetCotoId - id
-    )
+    val toDelete = (from(id).toSeq ++ to(id)).map(_.id)
+    toDelete.foldLeft(this)(_ delete _)
   }
 }
