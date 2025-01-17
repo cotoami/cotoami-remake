303e2d44eed319a2f12b68df1b156b9a53612bdc
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-01-17 23:23:41 +0900

    refactor(node): node_service_test::websocket_server

--- cotoami_node/tests/node_service_test.rs
+++ cotoami_node/tests/node_service_test.rs
@@ -1,11 +1,12 @@
 use std::{sync::Arc, time::Instant};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use cotoami_db::prelude::*;
 use cotoami_node::prelude::*;
 use futures::Stream;
 use googletest::prelude::*;
 use tempfile::tempdir;
+use tokio::sync::oneshot::Sender;
 
 async fn test_node_service<S, C>(
     service: &S,
@@ -16,6 +17,8 @@ where
     S: NodeService + ?Sized,
     C: Stream<Item = ChangelogEntry>,
 {
+    let service_node = ds.local_node()?;
+
     /////////////////////////////////////////////////////////////////////////////
     // Command: LocalNode
     /////////////////////////////////////////////////////////////////////////////
@@ -28,25 +31,22 @@ where
     let node = response.content::<Node>()?;
     assert_that!(
         node,
-        pat!(Node {
-            name: eq("Cotoami")
+        eq(&Node {
+            rowid: 0, // skip_deserializing
+            ..service_node.clone()
         })
     );
 
     Ok(())
 }
 
-fn default_config() -> Result<NodeConfig> {
-    let db_dir = tempdir()?;
-    Ok(NodeConfig::new_standalone(
-        Some(db_dir.path().to_string_lossy().into()),
-        Some("Cotoami".into()),
-    ))
-}
+/////////////////////////////////////////////////////////////////////////////
+// Local node as a service
+/////////////////////////////////////////////////////////////////////////////
 
 #[tokio::test]
 async fn local_node() -> Result<()> {
-    let config = default_config()?;
+    let config = new_node_config()?;
     let state = NodeState::new(config).await?;
     let mut ds = state.db().new_session()?;
     let changes = state.pubsub().changes().subscribe(None::<()>);
@@ -54,54 +54,86 @@ async fn local_node() -> Result<()> {
     test_node_service(&state, &mut ds, changes).await
 }
 
+/////////////////////////////////////////////////////////////////////////////
+// WebSocket server as a service
+/////////////////////////////////////////////////////////////////////////////
+
 #[tokio::test]
 async fn websocket_server() -> Result<()> {
     // Client node
-    let mut client_config = default_config()?;
-    client_config.owner_password = Some("master-password".into());
-    let client_state = NodeState::new(client_config).await?;
-    let client_opr = Arc::new(client_state.local_node_as_operator()?);
+    let client_state = new_client_node_state().await?;
 
     // Server node
-    let mut server_config = default_config()?;
-    server_config.owner_remote_node_id = Some(client_state.try_get_local_node_id()?);
-    server_config.owner_remote_node_password = Some("server-password".into());
-    let server_state = NodeState::new(server_config).await?;
+    let (server_state, _shutdown) =
+        launch_server_node(client_state.try_get_local_node_id()?).await?;
     let server_id = server_state.try_get_local_node_id()?;
-    let (_handle, _shutdown_trigger) =
-        cotoami_node::launch_server(ServerConfig::default(), server_state.clone()).await?;
 
     // Connect the client to the server
-    let mut request = Command::AddServer(LogIntoServer {
-        url_prefix: Some("http://localhost:5103".into()),
-        password: Some("server-password".into()),
-        new_password: None,
-        client_role: Some(NodeRole::Child),
-    })
-    .into_request();
-    request.set_from(client_opr.clone());
-    let response = client_state.call(request).await?;
-    let server = response.content::<Server>()?;
+    let server = connect_to_server(&client_state, NodeRole::Child).await?;
     assert_that!(server.server.node_id, eq(server_id));
 
     // Test the server service via the client node
-    let mut parent_service = client_state.parent_services().get(&server_id);
-    let start = Instant::now();
-    while parent_service.is_none() && start.elapsed().as_secs() < 10 {
-        tokio::task::yield_now().await;
-        parent_service = client_state.parent_services().get(&server_id);
-    }
+    let parent_service = get_parent_service(&client_state, &server_id).await?;
     let mut server_ds = server_state.db().new_session()?;
     let remote_changes = client_state
         .pubsub()
         .remote_changes()
         .subscribe(Some(server_state.try_get_local_node_id()?));
-    let _ = test_node_service(
-        parent_service.unwrap().as_ref(),
-        &mut server_ds,
-        remote_changes,
-    )
-    .await?;
+    let _ = test_node_service(parent_service.as_ref(), &mut server_ds, remote_changes).await?;
 
     Ok(())
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Utils
+/////////////////////////////////////////////////////////////////////////////
+
+fn new_node_config() -> Result<NodeConfig> {
+    let db_dir = tempdir()?;
+    Ok(NodeConfig::new_standalone(
+        Some(db_dir.path().to_string_lossy().into()),
+        Some("Cotoami".into()),
+    ))
+}
+
+async fn new_client_node_state() -> Result<NodeState> {
+    let mut node_config = new_node_config()?;
+    node_config.owner_password = Some("master-password".into());
+    NodeState::new(node_config).await
+}
+
+async fn launch_server_node(owner_remote_node_id: Id<Node>) -> Result<(NodeState, Sender<()>)> {
+    let mut node_config = new_node_config()?;
+    node_config.owner_remote_node_id = Some(owner_remote_node_id);
+    node_config.owner_remote_node_password = Some("server-password".into());
+    let server_state = NodeState::new(node_config).await?;
+    let (_, shutdown) =
+        cotoami_node::launch_server(ServerConfig::default(), server_state.clone()).await?;
+    Ok((server_state, shutdown))
+}
+
+async fn connect_to_server(client_state: &NodeState, role: NodeRole) -> Result<Server> {
+    let mut request = Command::AddServer(LogIntoServer {
+        url_prefix: Some("http://localhost:5103".into()),
+        password: Some("server-password".into()),
+        new_password: None,
+        client_role: Some(role),
+    })
+    .into_request();
+    request.set_from(Arc::new(client_state.local_node_as_operator()?));
+    let response = client_state.call(request).await?;
+    response.content::<Server>()
+}
+
+async fn get_parent_service(
+    client_state: &NodeState,
+    parent_id: &Id<Node>,
+) -> Result<Box<dyn NodeService>> {
+    let mut parent_service = client_state.parent_services().get(parent_id);
+    let start = Instant::now();
+    while parent_service.is_none() && start.elapsed().as_secs() < 10 {
+        tokio::task::yield_now().await;
+        parent_service = client_state.parent_services().get(parent_id);
+    }
+    parent_service.ok_or(anyhow!("Could not get the parent service: {parent_id}"))
 }
