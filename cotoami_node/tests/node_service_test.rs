0bf6d40d882a2684102368d99f164d0118a5b448
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-01-20 09:42:55 +0900

    refactor(node): rename a param in node_service_test::get_parent_service

--- cotoami_node/tests/node_service_test.rs
+++ cotoami_node/tests/node_service_test.rs
@@ -219,14 +219,14 @@ async fn test_service_based_on_remote_node(
 }
 
 async fn get_parent_service(
-    node_state: &NodeState,
+    child_state: &NodeState,
     parent_id: &Id<Node>,
 ) -> Result<Box<dyn NodeService>> {
-    let mut parent_service = node_state.parent_services().get(parent_id);
+    let mut parent_service = child_state.parent_services().get(parent_id);
     let start = Instant::now();
     while parent_service.is_none() && start.elapsed().as_secs() < 10 {
         tokio::task::yield_now().await;
-        parent_service = node_state.parent_services().get(parent_id);
+        parent_service = child_state.parent_services().get(parent_id);
     }
     parent_service.ok_or(anyhow!("Could not get the parent service: {parent_id}"))
 }
