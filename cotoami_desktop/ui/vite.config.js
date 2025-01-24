8e8116f3079e5060fd59eaa69f5717fa38257709
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-01-25 08:38:03 +0900

    chore(ui): testing usePolling in vite.config.js

--- cotoami_desktop/ui/vite.config.js
+++ cotoami_desktop/ui/vite.config.js
@@ -4,4 +4,10 @@ import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 export default defineConfig({
   publicDir: "assets/static",
   plugins: [scalaJSPlugin()],
+  server: {
+    watch: {
+      // Testing this option because watch stopped working on macOS.
+      usePolling: true
+    }
+  }
 });
