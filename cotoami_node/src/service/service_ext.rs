84141bc0ffa171d8f0e36510e6512b400d9193cb
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-02-16 00:36:45 +0900

    fix(node): anonymous support in RemoteNodeServiceExt::create_client_node_session

--- cotoami_node/src/service/service_ext.rs
+++ cotoami_node/src/service/service_ext.rs
@@ -108,7 +108,9 @@ pub trait RemoteNodeServiceExt: RemoteNodeService {
         let request = Command::CreateClientNodeSession(input).into_request();
         let response = self.call(request).await?;
         let client_node_session = response.content::<ClientNodeSession>()?;
-        self.set_session_token(&client_node_session.session.token)?;
+        if let Some(ref session) = client_node_session.session {
+            self.set_session_token(&session.token)?;
+        }
         Ok(client_node_session)
     }
 }
