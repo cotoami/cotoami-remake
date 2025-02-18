32e9338cf09036e4a06c9782b62ae37829a63c6f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-02-18 18:16:52 +0900

    fix(ui): add permission check in ToolbarReorder

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/ToolbarReorder.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/ToolbarReorder.scala
@@ -16,50 +16,53 @@ object ToolbarReorder {
   )(implicit
       context: Context,
       dispatch: Into[AppMsg] => Unit
-  ): ReactElement = {
-    val reordering = context.repo.reordering.contains(link.sourceCotoId)
-    section(className := "reorder-toolbar")(
-      toolButton(
-        classes = "move-to-top",
-        symbol = "vertical_align_top",
-        disabled = order.isFirst || reordering,
-        onClick = e => {
-          e.stopPropagation()
-          dispatch(Root.Msg.ChangeOrder(link, 1))
+  ): Option[ReactElement] =
+    Option.when(context.repo.nodes.canCreateLinksIn(link.nodeId)) {
+      val reordering = context.repo.reordering.contains(link.sourceCotoId)
+      section(className := "reorder-toolbar")(
+        toolButton(
+          classes = "move-to-top",
+          symbol = "vertical_align_top",
+          disabled = order.isFirst || reordering,
+          onClick = e => {
+            e.stopPropagation()
+            dispatch(Root.Msg.ChangeOrder(link, 1))
+          }
+        ),
+        toolButton(
+          classes = "move-up",
+          symbol = "arrow_upward",
+          disabled = order.isFirst || reordering,
+          onClick = e => {
+            e.stopPropagation()
+            order.previous.map(previous =>
+              dispatch(Root.Msg.ChangeOrder(link, previous))
+            )
+          }
+        ),
+        toolButton(
+          classes = "move-down",
+          symbol = "arrow_downward",
+          disabled = order.isLast || reordering,
+          onClick = e => {
+            e.stopPropagation()
+            order.next.map(next =>
+              dispatch(Root.Msg.ChangeOrder(link, next + 1))
+            )
+          }
+        ),
+        toolButton(
+          classes = "move-to-bottom",
+          symbol = "vertical_align_bottom",
+          disabled = order.isLast || reordering,
+          onClick = e => {
+            e.stopPropagation()
+            dispatch(Root.Msg.ChangeOrder(link, order.max + 1))
+          }
+        ),
+        Option.when(reordering) {
+          span(className := "reordering", aria - "busy" := "true")()
         }
-      ),
-      toolButton(
-        classes = "move-up",
-        symbol = "arrow_upward",
-        disabled = order.isFirst || reordering,
-        onClick = e => {
-          e.stopPropagation()
-          order.previous.map(previous =>
-            dispatch(Root.Msg.ChangeOrder(link, previous))
-          )
-        }
-      ),
-      toolButton(
-        classes = "move-down",
-        symbol = "arrow_downward",
-        disabled = order.isLast || reordering,
-        onClick = e => {
-          e.stopPropagation()
-          order.next.map(next => dispatch(Root.Msg.ChangeOrder(link, next + 1)))
-        }
-      ),
-      toolButton(
-        classes = "move-to-bottom",
-        symbol = "vertical_align_bottom",
-        disabled = order.isLast || reordering,
-        onClick = e => {
-          e.stopPropagation()
-          dispatch(Root.Msg.ChangeOrder(link, order.max + 1))
-        }
-      ),
-      Option.when(reordering) {
-        span(className := "reordering", aria - "busy" := "true")()
-      }
-    )
-  }
+      )
+    }
 }
