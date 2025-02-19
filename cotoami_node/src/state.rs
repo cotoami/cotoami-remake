d426de3780708cc2f9457b4dfb21600c2c6e90c4
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-02-19 17:09:55 +0900

    refactor(node): move AnonymousConnection to state::client_conn

--- cotoami_node/src/state.rs
+++ cotoami_node/src/state.rs
@@ -7,7 +7,7 @@ use anyhow::{anyhow, bail, Result};
 use cotoami_db::prelude::*;
 use parking_lot::RwLock;
 use tokio::{sync::oneshot::Sender, task::JoinHandle};
-use tracing::{debug, error};
+use tracing::debug;
 use uuid::Uuid;
 use validator::Validate;
 
@@ -201,55 +201,5 @@ impl ParentServices {
     fn remove(&self, parent_id: &Id<Node>) -> Option<Box<dyn NodeService>> {
         debug!("Parent service being removed: {parent_id}");
         self.0.write().remove(parent_id)
-    }
-}
-
-#[derive(Debug)]
-pub struct AnonymousConnection {
-    remote_addr: String,
-    disconnect: Sender<()>,
-}
-
-impl AnonymousConnection {
-    pub fn new(remote_addr: impl Into<String>, disconnect: Sender<()>) -> Self {
-        Self {
-            remote_addr: remote_addr.into(),
-            disconnect,
-        }
-    }
-
-    pub fn remote_addr(&self) -> &str { &self.remote_addr }
-
-    pub fn disconnect(self) {
-        let Self {
-            remote_addr,
-            disconnect,
-        } = self;
-        if let Err(e) = disconnect.send(()) {
-            error!("Error disconnecting {remote_addr}: {e:?}");
-        }
-    }
-}
-
-#[derive(Clone, Default)]
-pub struct AnonymousConnections(
-    #[allow(clippy::type_complexity)] Arc<RwLock<HashMap<Uuid, AnonymousConnection>>>,
-);
-
-impl AnonymousConnections {
-    pub fn count(&self) -> usize { self.0.read().len() }
-
-    fn add(&self, conn: AnonymousConnection) -> Uuid {
-        let id = Uuid::now_v7();
-        self.0.write().insert(id, conn);
-        id
-    }
-
-    fn remove(&self, id: &Uuid) -> Option<AnonymousConnection> { self.0.write().remove(id) }
-
-    pub(crate) fn disconnect_all(&self) {
-        for (_, conn) in self.0.write().drain() {
-            conn.disconnect();
-        }
     }
 }
--- cotoami_node/src/state/client_conn.rs
+++ cotoami_node/src/state/client_conn.rs
@@ -4,6 +4,7 @@ use cotoami_db::prelude::*;
 use parking_lot::RwLock;
 use tokio::sync::oneshot::Sender;
 use tracing::error;
+use uuid::Uuid;
 
 use crate::service::models::ActiveClient;
 
@@ -77,5 +78,65 @@ impl ClientConnections {
             .values()
             .map(|conn| conn.client.clone())
             .collect()
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// AnonymousConnection
+/////////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug)]
+pub struct AnonymousConnection {
+    remote_addr: String,
+    disconnect: Sender<()>,
+}
+
+impl AnonymousConnection {
+    pub fn new(remote_addr: impl Into<String>, disconnect: Sender<()>) -> Self {
+        Self {
+            remote_addr: remote_addr.into(),
+            disconnect,
+        }
+    }
+
+    pub fn remote_addr(&self) -> &str { &self.remote_addr }
+
+    pub fn disconnect(self) {
+        let Self {
+            remote_addr,
+            disconnect,
+        } = self;
+        if let Err(e) = disconnect.send(()) {
+            error!("Error disconnecting {remote_addr}: {e:?}");
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// AnonymousConnections
+/////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Default)]
+pub struct AnonymousConnections(
+    #[allow(clippy::type_complexity)] Arc<RwLock<HashMap<Uuid, AnonymousConnection>>>,
+);
+
+impl AnonymousConnections {
+    pub fn count(&self) -> usize { self.0.read().len() }
+
+    pub(super) fn add(&self, conn: AnonymousConnection) -> Uuid {
+        let id = Uuid::now_v7();
+        self.0.write().insert(id, conn);
+        id
+    }
+
+    pub(super) fn remove(&self, id: &Uuid) -> Option<AnonymousConnection> {
+        self.0.write().remove(id)
+    }
+
+    pub(crate) fn disconnect_all(&self) {
+        for (_, conn) in self.0.write().drain() {
+            conn.disconnect();
+        }
     }
 }
