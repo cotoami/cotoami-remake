c8952f23def2df9ad216073dfeab7dfd098916b9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-02-28 13:15:34 +0900

    refactor(db): tweak comments in repost_test

--- cotoami_db/tests/repost_test.rs
+++ cotoami_db/tests/repost_test.rs
@@ -6,7 +6,10 @@ pub mod common;
 
 #[test]
 fn repost() -> Result<()> {
+    /////////////////////////////////////////////////////////////////////////////
     // Setup
+    /////////////////////////////////////////////////////////////////////////////
+
     let (_root_dir, db, node) = common::setup_db("My Node")?;
     let mut ds = db.new_session()?;
     let opr = db.globals().local_node_as_operator()?;
@@ -17,7 +20,10 @@ fn repost() -> Result<()> {
     let ((cotonoma2, _), _) =
         ds.post_cotonoma(&CotonomaInput::new("Package manager"), &root, &opr)?;
 
+    /////////////////////////////////////////////////////////////////////////////
     // When: repost a coto
+    /////////////////////////////////////////////////////////////////////////////
+
     let ((repost1, original), changelog) = ds.repost(&coto.uuid, &cotonoma1, &opr)?;
 
     assert_that!(
@@ -61,7 +67,10 @@ fn repost() -> Result<()> {
         })
     );
 
+    /////////////////////////////////////////////////////////////////////////////
     // When: repost a repost
+    /////////////////////////////////////////////////////////////////////////////
+
     let ((repost2, original), changelog) = ds.repost(&repost1.uuid, &cotonoma2, &opr)?;
 
     assert_that!(
@@ -104,7 +113,10 @@ fn repost() -> Result<()> {
         })
     );
 
-    // When: delete one of the two reposts
+    /////////////////////////////////////////////////////////////////////////////
+    // When: delete a repost
+    /////////////////////////////////////////////////////////////////////////////
+
     let ChangelogEntry {
         change: Change::DeleteCoto { deleted_at, .. },
         ..
@@ -113,7 +125,7 @@ fn repost() -> Result<()> {
         panic!("Unexpected changelog returned from delete_coto.");
     };
 
-    // Then: expect the `reposted_in_ids` to be updated
+    // `reposted_in_ids` should be updated
     assert_eq!(ds.coto(&repost2.uuid)?, None);
     assert_that!(
         ds.coto(&coto.uuid)?,
@@ -125,19 +137,25 @@ fn repost() -> Result<()> {
         }))
     );
 
+    /////////////////////////////////////////////////////////////////////////////
     // When: delete the original coto
+    /////////////////////////////////////////////////////////////////////////////
+
     let _ = ds.delete_coto(&coto.uuid, &opr)?;
 
-    // Then: expect cascading delete
+    // Expect cascading delete
     assert_eq!(ds.coto(&coto.uuid)?, None);
     assert_eq!(ds.coto(&repost1.uuid)?, None);
 
+    /////////////////////////////////////////////////////////////////////////////
     // When: delete the only repost
+    /////////////////////////////////////////////////////////////////////////////
+
     let (coto, _) = ds.post_coto(&CotoInput::new("Hello"), &root.uuid, &opr)?;
     let ((repost, _), _) = ds.repost(&coto.uuid, &cotonoma1, &opr)?;
     let _ = ds.delete_coto(&repost.uuid, &opr)?;
 
-    // Then: expect `reposted_in_ids` should be updated to be null
+    // `reposted_in_ids` should be updated to be null
     assert_eq!(ds.coto(&repost.uuid)?, None);
     assert_that!(
         ds.coto(&coto.uuid)?,
