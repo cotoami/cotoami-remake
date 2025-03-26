0050fb8db4f9710195aba1b640d630fa9dc46779
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-03-26 13:55:26 +0900

    test(db): common::setup_db_with_password

--- cotoami_db/tests/common.rs
+++ cotoami_db/tests/common.rs
@@ -3,11 +3,18 @@ use cotoami_db::{prelude::*, time};
 use tempfile::{tempdir, NamedTempFile, TempDir, TempPath};
 
 pub fn setup_db<'a>(name: &str) -> Result<(TempDir, Database, Node)> {
+    setup_db_with_password(name, None)
+}
+
+pub fn setup_db_with_password<'a>(
+    name: &str,
+    password: Option<&str>,
+) -> Result<(TempDir, Database, Node)> {
     time::clear_mock_time();
     let root_dir = tempdir()?;
     let db = Database::new(&root_dir)?;
     let session = db.new_session()?;
-    let ((_, node), _) = session.init_as_node(Some(name), None)?;
+    let ((_, node), _) = session.init_as_node(Some(name), password)?;
     drop(session);
     Ok((root_dir, db, node))
 }
