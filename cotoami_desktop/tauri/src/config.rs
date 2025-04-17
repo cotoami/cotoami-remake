de7ac31717c71ff11aa646b63078318953304148
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-04-17 17:19:12 +0900

    test(tauri): fix a failing test

--- cotoami_desktop/tauri/src/config.rs
+++ cotoami_desktop/tauri/src/config.rs
@@ -91,14 +91,12 @@ mod tests {
                 node_name = "Hello"
                 session_minutes = 1440
                 changes_chunk_size = 100
-                image_max_size = 1200
 
                 [00000000-0000-0000-0000-000000000002]
                 db_dir = "/path/to/db2"
                 node_name = "Bye"
                 session_minutes = 1440
                 changes_chunk_size = 100
-                image_max_size = 1200
             "#})
         );
         Ok(())
