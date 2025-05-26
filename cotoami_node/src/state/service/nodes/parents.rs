2016d09692a497868f668911c94dbaed91ea9762
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-05-26 12:49:51 +0900

    Add NodeState::others_last_posted_at

--- cotoami_node/src/state/service/nodes/parents.rs
+++ cotoami_node/src/state/service/nodes/parents.rs
@@ -1,6 +1,7 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::Result;
+use chrono::NaiveDateTime;
 use cotoami_db::prelude::*;
 
 use crate::{service::ServiceError, state::NodeState};
@@ -11,5 +12,13 @@ impl NodeState {
         operator: Arc<Operator>,
     ) -> Result<Vec<ParentNode>, ServiceError> {
         self.get(move |ds| ds.parent_nodes(&operator)).await
+    }
+
+    pub async fn others_last_posted_at(
+        &self,
+        operator: Arc<Operator>,
+    ) -> Result<HashMap<Id<Node>, Option<NaiveDateTime>>, ServiceError> {
+        self.get(move |ds| ds.others_last_posted_at(&operator))
+            .await
     }
 }
