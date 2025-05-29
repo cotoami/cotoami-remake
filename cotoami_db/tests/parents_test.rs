394128e62c26b5d1e148ddda0b1a2abc9074e7f2
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-05-29 20:13:04 +0900

    Update parents_test.rs

--- cotoami_db/tests/parents_test.rs
+++ cotoami_db/tests/parents_test.rs
@@ -10,9 +10,15 @@ fn parents() -> Result<()> {
     // Setup
     /////////////////////////////////////////////////////////////////////////////
 
-    let (_sun_dir, sun_db, _sun_node) = common::setup_db("Sun")?;
-    let (_moon_dir, moon_db, _moon_node) = common::setup_db("Moon")?;
+    let (_sun_dir, sun_db, sun_node) = common::setup_db("Sun")?;
+    let (_moon_dir, moon_db, moon_node) = common::setup_db("Moon")?;
     let (_earth_dir, earth_db, _earth_node) = common::setup_db("Earth")?;
+
+    let mut sun_ds = sun_db.new_session()?;
+    let (_sun_root_cotonoma, sun_root_coto) = sun_ds.try_get_local_node_root()?;
+
+    let mut moon_ds = moon_db.new_session()?;
+    let (_moon_root_cotonoma, moon_root_coto) = moon_ds.try_get_local_node_root()?;
 
     let mut earth_ds = earth_db.new_session()?;
     let earth_opr = earth_db.globals().local_node_as_operator()?;
@@ -31,6 +37,24 @@ fn parents() -> Result<()> {
         "moon-earth-password",
         ChildNodeInput::as_owner(),
     )?;
+    common::connect_parent_child(
+        &sun_db,
+        &moon_db,
+        "http://sun",
+        "sun-moon-password",
+        ChildNodeInput::as_owner(),
+    )?;
+
+    let others_last_posted_at = earth_ds.others_last_posted_at(&earth_opr)?;
+    assert_that!(others_last_posted_at.len(), eq(2));
+    assert_that!(
+        others_last_posted_at,
+        has_entry(sun_node.uuid, eq(&sun_root_coto.created_at))
+    );
+    assert_that!(
+        others_last_posted_at,
+        has_entry(moon_node.uuid, eq(&moon_root_coto.created_at))
+    );
 
     /////////////////////////////////////////////////////////////////////////////
     // When: parent_nodes
@@ -47,6 +71,28 @@ fn parents() -> Result<()> {
                 node_id: eq(&sun_db.globals().try_get_local_node_id()?)
             })
         ]
+    );
+
+    /////////////////////////////////////////////////////////////////////////////
+    // When: mark_as_read
+    /////////////////////////////////////////////////////////////////////////////
+
+    let read_at = earth_ds.mark_as_read(&sun_node.uuid, &earth_opr)?;
+    let parent_node = earth_ds.parent_node(&sun_node.uuid, &earth_opr)?;
+
+    assert_that!(
+        parent_node,
+        some(pat!(ParentNode {
+            node_id: eq(&sun_node.uuid),
+            last_read_at: some(eq(&read_at)),
+        }))
+    );
+
+    let others_last_posted_at = earth_ds.others_last_posted_at(&earth_opr)?;
+    assert_that!(others_last_posted_at.len(), eq(1));
+    assert_that!(
+        others_last_posted_at,
+        has_entry(moon_node.uuid, eq(&moon_root_coto.created_at))
     );
 
     Ok(())
