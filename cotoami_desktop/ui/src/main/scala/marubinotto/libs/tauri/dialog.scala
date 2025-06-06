7f38adef365347c97534784b1f705294f88c140e
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-06-06 18:13:32 +0900

    fix(tauri): migrate to plugin-dialog

--- cotoami_desktop/ui/src/main/scala/marubinotto/libs/tauri/dialog.scala
+++ cotoami_desktop/ui/src/main/scala/marubinotto/libs/tauri/dialog.scala
@@ -5,7 +5,7 @@ import scala.scalajs.js.|
 import scala.scalajs.js.annotation.JSImport
 
 @js.native
-@JSImport("@tauri-apps/api/dialog", JSImport.Namespace)
+@JSImport("@tauri-apps/plugin-dialog", JSImport.Namespace)
 object dialog extends js.Object {
 
   /** Open a file/directory selection dialog.
@@ -28,32 +28,56 @@ object dialog extends js.Object {
       options: OpenDialogOptions
   ): js.Promise[scala.Null | String | js.Array[String]] = js.native
 
+  /** Extension filters for the file dialog.
+    * https://v2.tauri.app/reference/javascript/dialog/#dialogfilter
+    */
   trait DialogFilter extends js.Object {
-    // Extensions to filter, without a . prefix.
+
+    /** Extensions to filter, without a . prefix.
+      */
     val extensions: js.Array[String]
 
-    // Filter name.
+    /** Filter name.
+      */
     val name: String
   }
 
+  /** Options for the open dialog.
+    * https://v2.tauri.app/reference/javascript/dialog/#opendialogoptions
+    */
   trait OpenDialogOptions extends js.Object {
-    // Initial directory or file path.
+
+    /** Whether to allow creating directories in the dialog. Enabled by default.
+      * macOS Only.
+      */
+    val canCreateDirectories: js.UndefOr[Boolean] = js.undefined
+
+    /** Initial directory or file path. If it’s a directory path, the dialog
+      * interface will change to that folder. If it’s not an existing directory,
+      * the file name will be set to the dialog’s file name input and the dialog
+      * will be set to the parent folder.
+      */
     val defaultPath: js.UndefOr[String] = js.undefined
 
-    // Whether the dialog is a directory selection or not.
+    /** Whether the dialog is a directory selection or not.
+      */
     val directory: js.UndefOr[Boolean] = js.undefined
 
-    // Whether the dialog allows multiple selection or not.
+    /** The filters of the dialog.
+      */
+    val filters: js.UndefOr[js.Array[DialogFilter]] = js.undefined
+
+    /** Whether the dialog allows multiple selection or not.
+      */
     val multiple: js.UndefOr[Boolean] = js.undefined
 
-    // If directory is true, indicates that it will be read recursively later.
-    // Defines whether subdirectories will be allowed on the scope or not.
+    /** If directory is true, indicates that it will be read recursively later.
+      * Defines whether subdirectories will be allowed on the scope or not.
+      */
     val recursive: js.UndefOr[Boolean] = js.undefined
 
-    // The title of the dialog window.
+    /** The title of the dialog window (desktop only).
+      */
     val title: js.UndefOr[String] = js.undefined
-
-    // The filters of the dialog.
-    val filters: js.UndefOr[js.Array[DialogFilter]] = js.undefined
   }
 }
