6eb70130258323b4da4da3b8312b87eeb2620457
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-06-14 07:36:50 +0900

    feat(db): DatabaseSession::incoming_neighbors()

--- cotoami_db/src/db/transactions/graph.rs
+++ cotoami_db/src/db/transactions/graph.rs
@@ -1,7 +1,12 @@
 use anyhow::Result;
+use diesel::sqlite::SqliteConnection;
 
 use crate::{
-    db::{ops::graph_ops, DatabaseSession},
+    db::{
+        op::*,
+        ops::{coto_ops, graph_ops, ito_ops},
+        DatabaseSession,
+    },
     models::prelude::*,
 };
 
@@ -12,5 +17,15 @@ impl DatabaseSession<'_> {
 
     pub fn graph_by_cte(&mut self, root: Coto, until_cotonoma: bool) -> Result<Graph> {
         self.read_transaction(graph_ops::traverse_by_recursive_cte(root, until_cotonoma))
+    }
+
+    pub fn incoming_neighbors(&mut self, coto_id: &Id<Coto>) -> Result<(Vec<Ito>, Vec<Coto>)> {
+        self.read_transaction(|ctx: &mut Context<'_, SqliteConnection>| {
+            let itos = ito_ops::incoming(coto_id).run(ctx)?;
+            let source_coto_ids: Vec<Id<Coto>> =
+                itos.iter().map(|ito| ito.source_coto_id).collect();
+            let cotos = coto_ops::get_by_ids(&source_coto_ids).run(ctx)?;
+            Ok((itos, cotos))
+        })
     }
 }
