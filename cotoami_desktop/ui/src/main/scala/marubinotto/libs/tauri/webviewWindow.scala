6ca3727110c0445ee9fd3bfd1f2bd23e02beb1ef
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-06-17 10:03:04 +0900

    feat(ui): marubinotto.libs.tauri.window

--- cotoami_desktop/ui/src/main/scala/marubinotto/libs/tauri/webviewWindow.scala
+++ cotoami_desktop/ui/src/main/scala/marubinotto/libs/tauri/webviewWindow.scala
@@ -1,10 +1,7 @@
 package marubinotto.libs.tauri
 
 import scala.scalajs.js
-import scala.scalajs.js.|
 import scala.scalajs.js.annotation.JSImport
-
-import marubinotto.libs.tauri.dpi._
 
 @js.native
 @JSImport("@tauri-apps/api/webviewWindow", JSImport.Namespace)
@@ -21,53 +18,11 @@ object webviewWindow extends js.Object {
     * plus the following special characters -, /, : and _.
     */
   @js.native
-  class WebviewWindow(label: String, options: WindowOptions) extends js.Object {
-
-    /** Centers the window.
-      */
-    def center(): js.Promise[Unit] = js.native
+  class WebviewWindow(label: String, options: WindowOptions)
+      extends window.Window(label, options) {
 
     /** Clears all browsing data for this webview.
       */
     def clearAllBrowsingData(): js.Promise[Unit] = js.native
-
-    /** Clear any applied effects if possible.
-      */
-    def clearEffects(): js.Promise[Unit] = js.native
-
-    /** Closes the webview.
-      */
-    def close(): js.Promise[Unit] = js.native
-
-    /** Destroys the window. Behaves like Window.close but forces the window
-      * close instead of emitting a closeRequested event.
-      */
-    def destroy(): js.Promise[Unit] = js.native
-
-    /** Hide the webview.
-      */
-    def hide(): js.Promise[Unit] = js.native
-
-    /** The physical size of the window's client area. The client area is the
-      * content of the window, excluding the title bar and borders.
-      */
-    def innerSize(): js.Promise[PhysicalSize] = js.native
-
-    /** The scale factor that can be used to map physical pixels to logical
-      * pixels.
-      */
-    def scaleFactor(): js.Promise[Double] = js.native
-
-    /** Resizes the window with a new inner size.
-      */
-    def setSize(size: PhysicalSize | LogicalSize): js.Promise[Unit] = js.native
   }
-}
-
-trait WindowOptions extends js.Object {
-
-  /** Whether clicking an inactive window also clicks through to the webview on
-    * macOS.
-    */
-  val acceptFirstMouse: js.UndefOr[Boolean] = js.undefined
 }
