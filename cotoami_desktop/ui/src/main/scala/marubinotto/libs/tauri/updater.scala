b62a47604a4164facaa21d95ff0f5e723fe6702f
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-06-20 11:20:37 +0900

    Add marubinotto.libs.tauri.updater.DownloadOptions

--- cotoami_desktop/ui/src/main/scala/marubinotto/libs/tauri/updater.scala
+++ cotoami_desktop/ui/src/main/scala/marubinotto/libs/tauri/updater.scala
@@ -14,4 +14,32 @@ object updater extends js.Object {
     val date: js.UndefOr[String] = js.native
     val body: js.UndefOr[String] = js.native
   }
+
+  /** Options used when downloading an update
+    */
+  trait DownloadOptions extends js.Object {
+
+    /** Timeout in milliseconds
+      */
+    val timeout: js.UndefOr[Double] = js.undefined
+  }
+
+  @js.native
+  trait DownloadEvent extends js.Object {
+
+    /** Event type: 'Started', 'Progress', 'Finished'
+      */
+    val event: String = js.native
+
+    val data: js.UndefOr[DownloadEventData] = js.native
+  }
+
+  @js.native
+  trait DownloadEventData extends js.Object {
+    // When 'Started'
+    val contentLength: js.UndefOr[Double] = js.native
+
+    // When 'Progress'
+    val chunkLength: js.UndefOr[Double] = js.native
+  }
 }
