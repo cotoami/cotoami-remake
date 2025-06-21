9d0ed039704c03b30e66a2a0db5d796c9629646c
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-06-21 12:52:49 +0900

    Add marubinotto.libs.tauri.process

--- cotoami_desktop/tauri/src/main.rs
+++ cotoami_desktop/tauri/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(
             tauri_plugin_window_state::Builder::default()
                 // https://github.com/tauri-apps/tauri/issues/1564#issuecomment-2642142824
