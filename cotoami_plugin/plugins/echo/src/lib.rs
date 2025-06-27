92059cc7f892be79686daa9d196c7c8f18d96176
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-06-27 12:08:57 +0900

    echo: set version from Cargo.toml

--- cotoami_plugin/plugins/echo/src/lib.rs
+++ cotoami_plugin/plugins/echo/src/lib.rs
@@ -3,8 +3,10 @@ use extism_pdk::*;
 
 #[plugin_fn]
 pub fn metadata() -> FnResult<Metadata> {
-    Ok(Metadata::new("app.cotoami.plugin.echo", "Echo", "0.1.0")
-        .mark_as_agent("Echo", Vec::from(include_bytes!("icon.png"))))
+    Ok(
+        Metadata::new("app.cotoami.plugin.echo", "Echo", env!("CARGO_PKG_VERSION"))
+            .mark_as_agent("Echo", Vec::from(include_bytes!("icon.png"))),
+    )
 }
 
 #[plugin_fn]
