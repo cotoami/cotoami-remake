a98b59f68aa136de792f16dd7f2e038660862f81
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-07-02 07:00:20 +0900

    Create cotoami_plugin/plugins/chatgpt

---
+++ cotoami_plugin/plugins/chatgpt/src/lib.rs
@@ -0,0 +1,51 @@
+use cotoami_plugin_api::*;
+use extism_pdk::*;
+use lazy_regex::*;
+
+const IDENTIFIER: &'static str = "app.cotoami.plugin.chatgpt";
+const NAME: &'static str = "ChatGPT";
+static COMMAND_PREFIX: Lazy<Regex> = lazy_regex!(r"^\s*\#chatgpt\s");
+
+#[host_fn]
+extern "ExtismHost" {
+    fn log(message: String);
+    fn version() -> String;
+    fn post_coto(input: CotoInput) -> Coto;
+}
+
+#[plugin_fn]
+pub fn metadata() -> FnResult<Metadata> {
+    Ok(Metadata::new(IDENTIFIER, NAME, env!("CARGO_PKG_VERSION"))
+        .mark_as_agent(NAME, Vec::from(include_bytes!("icon.png"))))
+}
+
+#[plugin_fn]
+pub fn init(config: Config) -> FnResult<()> {
+    let version = unsafe { version()? };
+    unsafe { log(format!("{IDENTIFIER}: node version: {version}"))? };
+    unsafe { log(format!("{IDENTIFIER}: init with: {config:?}"))? };
+    Ok(())
+}
+
+#[plugin_fn]
+pub fn on(event: Event) -> FnResult<()> {
+    match event {
+        Event::CotoPosted {
+            coto,
+            local_node_id,
+        } => {
+            let content = coto.content.unwrap_or_default();
+            if coto.node_id == local_node_id && COMMAND_PREFIX.is_match(&content) {
+                let echo = COMMAND_PREFIX.replace(&content, "").trim().to_owned();
+                let input = CotoInput::new(echo, Some(coto.posted_in_id));
+                unsafe { post_coto(input)? };
+            }
+        }
+    }
+    Ok(())
+}
+
+#[plugin_fn]
+pub fn destroy() -> FnResult<()> {
+    Ok(())
+}
