bf9840bad2f1a89358a61f17dc10fcf5c3490f10
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-07-20 07:30:51 +0900

    Add extract_query()

--- cotoami_plugins/geocoder/src/lib.rs
+++ cotoami_plugins/geocoder/src/lib.rs
@@ -1,8 +1,12 @@
+use anyhow::Result;
 use cotoami_plugin_api::*;
 use extism_pdk::*;
+use lazy_regex::*;
 
 const IDENTIFIER: &'static str = "app.cotoami.plugin.geocoder";
 const NAME: &'static str = "Geocoder";
+
+static QUERY_PATTERN: Lazy<Regex> = lazy_regex!(r"(?m)^#geocode\s(.*)$");
 
 #[host_fn]
 extern "ExtismHost" {
@@ -23,11 +27,48 @@ pub fn init(config: Config) -> FnResult<()> {
 }
 
 #[plugin_fn]
-pub fn on(_event: Event) -> FnResult<()> {
+pub fn on(event: Event) -> FnResult<()> {
+    match event {
+        Event::CotoPosted {
+            coto,
+            local_node_id,
+        } => {
+            geocode(&coto, &local_node_id)?;
+        }
+        Event::CotoUpdated {
+            coto,
+            local_node_id,
+        } => {
+            geocode(&coto, &local_node_id)?;
+        }
+    }
     Ok(())
 }
 
 #[plugin_fn]
 pub fn destroy() -> FnResult<()> {
     Ok(())
+}
+
+fn geocode(coto: &Coto, local_node_id: &str) -> Result<()> {
+    let Some(query) = extract_query(&coto, &local_node_id) else {
+        return Ok(());
+    };
+    unsafe { log(format!("{IDENTIFIER}: query: {query:?}"))? };
+    Ok(())
+}
+
+fn extract_query(coto: &Coto, local_node_id: &str) -> Option<String> {
+    if coto.node_id != local_node_id {
+        return None; // Ignore remote cotos
+    }
+    let Some(content) = &coto.content else {
+        return None; // No content
+    };
+    if let Some(caps) = QUERY_PATTERN.captures(content) {
+        if let Some(cap) = caps.get(1) {
+            return Some(cap.as_str().trim().to_owned());
+        }
+    }
+    None
 }
