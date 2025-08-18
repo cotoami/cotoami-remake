59b8cde268bf3dcffde700853baadf59f60a0ecc
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-08-18 16:40:27 +0900

    Tweak MasonryLayout

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/pins/MasonryLayout.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/pins/MasonryLayout.scala
@@ -1,16 +1,37 @@
 package cotoami.subparts.pins
 
 import slinky.core.facade.ReactElement
+import slinky.web.html._
 
+import marubinotto.optionalClasses
 import cotoami.{Context, Into, Msg => AppMsg}
 import cotoami.models.Siblings
+import cotoami.subparts.PartsNode
 
 object MasonryLayout {
 
   def apply(
       pins: Siblings
   )(implicit context: Context, dispatch: Into[AppMsg] => Unit): ReactElement =
-    sectionPinnedCotos(pins) { subCotos =>
-      cotoami.subparts.pins.sectionSubCotos(subCotos)
-    }
+    section(className := "pinned-cotos siblings")(
+      pins.groupsInOrder.map { group =>
+        section(
+          className := optionalClasses(
+            Seq(
+              ("sibling-group", true),
+              ("in-other-nodes", !group.isMain)
+            )
+          )
+        )(
+          Option.when(!group.isMain) {
+            context.repo.nodes.get(group.nodeId).map { node =>
+              div(className := "ito-node")(PartsNode.spanNode(node))
+            }
+          },
+          group.eachWithOrderContext.map { case (ito, coto, order) =>
+            sectionPinnedCoto(ito, coto, order)(sectionSubCotos)
+          }
+        )
+      }: _*
+    )
 }
