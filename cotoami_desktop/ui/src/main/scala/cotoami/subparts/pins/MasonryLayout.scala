7d31deb4012117d7ec5cf007052b7bd1477a10d9
Daisuke Morita <daisuke.marubinotto@gmail.com>
2025-08-19 16:49:34 +0900

    Slider to Change Masonry Column Width (#95)

--- cotoami_desktop/ui/src/main/scala/cotoami/subparts/pins/MasonryLayout.scala
+++ cotoami_desktop/ui/src/main/scala/cotoami/subparts/pins/MasonryLayout.scala
@@ -8,45 +8,69 @@ import slinky.web.html._
 import marubinotto.optionalClasses
 import marubinotto.components.MasonicMasonry
 import cotoami.{Context, Into, Msg => AppMsg}
-import cotoami.models.{Coto, Ito, OrderContext, Siblings}
-import cotoami.subparts.PartsNode
+import cotoami.models.{Coto, Cotonoma, Id, Ito, OrderContext, Siblings}
+import cotoami.subparts.{PartsNode, SectionPins}
 
 object MasonryLayout {
 
+  val MinColumnWidth = 100
+  val MaxColumnWidth = 1000
+
   def apply(
-      pins: Siblings
+      pins: Siblings,
+      cotonomaId: Id[Cotonoma],
+      columnWidth: Int
   )(implicit context: Context, dispatch: Into[AppMsg] => Unit): ReactElement =
     section(className := "pinned-cotos siblings")(
-      pins.groupsInOrder.map { group =>
-        section(
-          className := optionalClasses(
-            Seq(
-              ("sibling-group", true),
-              ("in-other-nodes", !group.isMain)
+      div(className := "column-width-slider")(
+        input(
+          `type` := "range",
+          min := MinColumnWidth.toString(),
+          max := MaxColumnWidth.toString(),
+          value := columnWidth.toString(),
+          onChange := (e => {
+            e.target.value.toIntOption.foreach(width =>
+              dispatch(SectionPins.Msg.SetMasonryColumnWidth(cotonomaId, width))
+            )
+          })
+        )
+      ),
+      div()(
+        pins.groupsInOrder.map { group =>
+          section(
+            className := optionalClasses(
+              Seq(
+                ("sibling-group", true),
+                ("in-other-nodes", !group.isMain)
+              )
+            )
+          )(
+            Option.when(!group.isMain) {
+              context.repo.nodes.get(group.nodeId).map { node =>
+                div(className := "ito-node")(PartsNode.spanNode(node))
+              }
+            },
+            MasonicMasonry(
+              items = group.eachWithOrderContext.map {
+                case (ito, coto, order) =>
+                  PinnedCoto(ito, coto, order).asInstanceOf[scala.Any]
+              }.toSeq.toJSArray,
+              render = props => {
+                val data = props.data.asInstanceOf[PinnedCoto]
+                sectionPinnedCoto(data.ito, data.coto, data.order)(
+                  sectionSubCotos
+                )
+              },
+              columnWidth = columnWidth,
+              columnGutter = Some(16),
+              rowGutter = Some(20),
+              // Disable the virtualization to ensure to display all the cotos
+              // https://github.com/jaredLunde/masonic/issues/120
+              overscanBy = Double.PositiveInfinity
             )
           )
-        )(
-          Option.when(!group.isMain) {
-            context.repo.nodes.get(group.nodeId).map { node =>
-              div(className := "ito-node")(PartsNode.spanNode(node))
-            }
-          },
-          MasonicMasonry(
-            items = group.eachWithOrderContext.map { case (ito, coto, order) =>
-              PinnedCoto(ito, coto, order).asInstanceOf[scala.Any]
-            }.toSeq.toJSArray,
-            render = props => {
-              val data = props.data.asInstanceOf[PinnedCoto]
-              sectionPinnedCoto(data.ito, data.coto, data.order)(
-                sectionSubCotos
-              )
-            },
-            columnWidth = Some(300),
-            columnGutter = Some(16),
-            rowGutter = Some(20)
-          )
-        )
-      }: _*
+        }: _*
+      )
     )
 
   case class PinnedCoto(
